36d7cc27e00408a2d77b1759e73dc7832baa0b57
Brian Mesick <bmesick@edx.org>
2021-11-18 15:50:53 -0500

    temp: Add a test call to demonstrate firing a Django signal from inside a Celery task.
    1- URL is called
    2- View fires a signal
    3- Signal handler queues a Celery task
    4- Celery task fires a different signal
    5- Second signal queues a different Celery task
    6- Second task shows its usual output

--- commerce_coordinator/apps/lms/urls.py
+++ commerce_coordinator/apps/lms/urls.py
@@ -3,11 +3,12 @@ LMS app URLs, in a real service these would be callback handlers
 """
 from django.conf.urls import url
 
-from commerce_coordinator.apps.lms.views import demo_purchase_complete, test_view
+from commerce_coordinator.apps.lms.views import demo_purchase_complete, test_celery_signal_view, test_view
 
 app_name = 'lms'
 urlpatterns = [
     # FIXME: Test URL should be removed post proof-of-concept.
     url(r'^test/', test_view, name='lms_test'),
+    url(r'^test_celery_signal/', test_celery_signal_view, name='lms_test_celery_signal'),
     url(r'^demo_purchase_complete/', demo_purchase_complete, name='lms_demo_purchase_complete'),
 ]
