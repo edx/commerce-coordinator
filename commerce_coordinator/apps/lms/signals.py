4e044baf4696258c0c32e4aa8883e7e267286d02
Brian Mesick <bmesick@edx.org>
2021-11-18 15:07:19 -0500

    temp: Add demo call that shows a two-step workflow and fan out
    Adding in a demo "purchase complete" callback handler that shows a relatively plausible scenario:
    
    - CC gets a callback from a payment processor that a purchase went through, with some basic data indicating a fake bundle purchase
    - Callback fires off a "purchase complete" signal
    - Signal handler for a fake "order history" component gets called, firing off a downstream stub Celery task
    - Signal handler for a fake "email confirmation" component gets called, firing off a downstream stub Celery task
    - Signal handler for LMS "enroll user in courses from purchase" gets called
    - LMS "enroll user in courses from purchase" handler fires off a new "enroll user in single course" signal for each course in the bundle (yes this is not how it works, roll with it)
    - LMS "enroll user in single course" handler fires off a downstream stub Celery task mimicking calling the LMS API once per course

--- commerce_coordinator/apps/lms/signals.py
+++ commerce_coordinator/apps/lms/signals.py
@@ -3,8 +3,69 @@ LMS app signals and receivers.
 """
 import logging
 
+from commerce_coordinator.apps.core.signal_helpers import CoordinatorSignal
+from commerce_coordinator.apps.lms.tasks import (
+    demo_order_complete_send_confirmation_email_task,
+    demo_order_complete_send_enroll_in_course_task,
+    demo_order_complete_send_order_history_task
+)
+
 logger = logging.getLogger(__name__)
 
+#############################################################
+# FIXME: Proof-of-concept test code from here to end of file
+#############################################################
 
+
+# Basic signal receiver to test out connecting signals and handlers via config
 def test_receiver(sender, **kwargs):
     logger.info(f"LMS TEST_RECEIVER CALLED with sender '{sender}'!")
+
+
+# The rest of this file is related to the "Purchase Complete" demo
+purchase_complete_signal = CoordinatorSignal()
+enroll_learner_signal = CoordinatorSignal()
+
+
+def demo_purchase_complete_order_history(sender, **kwargs):
+    """
+    This signal receiver would typically be in a separate app for just the Order History service, but is here for
+    convenience. It kicks off a Celery task that would normally make an API to a 3rd party order history service.
+    """
+    logger.info(f"LMS demo_purchase_complete_order_history CALLED with sender '{sender}' and {kwargs}")
+    demo_order_complete_send_order_history_task.delay(kwargs['order_results'])
+
+
+def demo_purchase_complete_send_confirmation_email(sender, **kwargs):
+    """
+    This signal receiver would typically be in a separate app for just the email service, but is here for
+    convenience. It kicks off a Celery task that would normally make an API to a 3rd party email service to send
+    an order confirmation.
+    """
+    logger.info(f"LMS demo_purchase_complete_send_confirmation_email CALLED with sender '{sender}' and {kwargs}")
+    demo_order_complete_send_confirmation_email_task.delay(kwargs['order_results'])
+
+
+def demo_purchase_complete_enroll_in_course(sender, **kwargs):
+    """
+    This signal receiver is one that legitimately belongs in LMS, it first off an enrollment event for each purchased
+    course in an order. Any number of signal handlers could care about that, but in this demo only
+    demo_enroll_learner_in_course is hooked up.
+    """
+    logger.info(f"LMS demo_purchase_complete_enroll_in_course CALLED with sender '{sender}' and {kwargs}")
+
+    user_id = kwargs['order_results']['user_id']
+    for product_id in kwargs['order_results']['products']:
+        enroll_learner_signal.send_robust('demo_purchase_complete', user_id=user_id, product_id=product_id)
+
+
+def demo_enroll_learner_in_course(sender, **kwargs):
+    """
+    This signal receiver is one that legitimately belongs in LMS, it would kick off a Celery task to LMS to enroll a
+    user in a single course.
+    """
+    logger.info(f"LMS demo_purchase_complete_enroll_in_course CALLED with sender '{sender}' and {kwargs}")
+
+    user_id = kwargs['user_id']
+    product_id = kwargs['product_id']
+    demo_order_complete_send_enroll_in_course_task.delay(user_id, product_id)
--- commerce_coordinator/apps/lms/views.py
+++ commerce_coordinator/apps/lms/views.py
@@ -8,21 +8,13 @@ from django.http import JsonResponse
 
 from commerce_coordinator.apps.core.signals import test_signal
 
+from .signals import purchase_complete_signal
 
-def test_view(_):
+
+def _format_signal_send_response(results):
     """
-    FIXME: Test view for the proof-of-concept. It exists simply to show that we can call signals mapped in settings
-    and how we can potentially handle responses / exceptions from receivers.
-
-    Returns a JSON object with the results of our signal call, including string formatting traceback on an exception.
+    Takes the return value from a signal send_robust and returns a JSON HTTP response
     """
-    # This should fail, we don't allow a non-robust send!
-    # test_signal.send("Something")
-
-    # send_robust() will ensure all receivers are called, as opposed to send() which will return immediately upon
-    # any failure, causing some receivers to never be called.
-    results = test_signal.send_robust("Something")
-
     # The results of a send_robust are a tuple of a reference to the method called and the exception, if one was raised
     data = {}
     for receiver, response in results:
@@ -40,3 +32,54 @@ def test_view(_):
         data.update(result_dict)
 
     return JsonResponse(data)
+
+
+def test_view(_):
+    """
+    FIXME: Test view for the proof-of-concept. It exists simply to show that we can call signals mapped in settings
+    and how we can potentially handle responses / exceptions from receivers.
+
+    Returns a JSON object with the results of our signal call, including string formatting traceback on an exception.
+    """
+    # This should fail, we don't allow a non-robust send!
+    # test_signal.send("Something")
+
+    # send_robust() will ensure all receivers are called, as opposed to send() which will return immediately upon
+    # any failure, causing some receivers to never be called.
+    results = test_signal.send_robust("Something")
+
+    return _format_signal_send_response(results)
+
+
+def demo_purchase_complete(_):
+    """
+    Demonstrate a complex workflow that utilizes both fanout and serial steps.
+
+    This call kicks off a stubbed out version of what might happen when we get a callback from a payment provider for
+    a purchase of a bundle of courses. It fires a single signal, which has a listeners that represent several downstream
+    services. Some of those services (order history) are only hit once per order, some (lms) get a call for each course
+    in the bundle.
+    """
+    # Normally this data would come from a POST callback from a payment provider, but I'm putting it in this format
+    # for simplicity of testing and to make the structure obvious.
+    callback_results = {
+        'order_id': 'EDX-12345',
+        'user_id': 1313,
+        'status': 'success',
+        'cost': 1.00,
+        'products': [
+            'SKU123',
+            'SKU456',
+            'SKU789'
+        ]
+    }
+
+    # Send the signal for anything that cares about an order (as defined in settings). This should be three receivers:
+    # - Order history (kicks off a celery task to make an API call)
+    # - Email service (confirmation email gets sent)
+    # - LMS (enerolls the learner in all 3 courses by firing an enrollment signal for each course, which has a secondary
+    #       handler that kicks off a Celery task which sends an LMS API call for each)
+
+    results = purchase_complete_signal.send_robust('demo_purchase_complete', order_results=callback_results)
+
+    return _format_signal_send_response(results)
