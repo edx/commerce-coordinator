d7009554066af4a587767f3559a2ecb235770c5a
Ben Holt <bholt+github@edx.org>
2022-02-01 16:21:23 -0500

    feat: added a management command to start celery with auto-reload

---
+++ commerce_coordinator/apps/core/management/commands/celery.py
@@ -0,0 +1,20 @@
+import subprocess
+
+from django.core.management.base import BaseCommand
+from django.utils import autoreload
+
+
+def restart_celery():
+    cmd = ('pkill', 'celery')
+    subprocess.call(cmd)
+    cmd = ('celery', '-A', 'commerce_coordinator', 'worker', '-l', 'INFO')
+    subprocess.call(cmd)
+
+
+class Command(BaseCommand):
+
+    help = 'Starts a commerce-coordiantor celery worker with auto-reload.'
+
+    def handle(self, *args, **options):
+        print('Starting celery worker with auto-reload...')
+        autoreload.run_with_reloader(restart_celery) 
