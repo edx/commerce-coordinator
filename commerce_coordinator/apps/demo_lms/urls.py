b263b984d9225e445a010c71d757339ba3be6dbb
Mohammad Ahtasham ul Hassan <ahthassan74@gmail.com>
2022-02-16 15:47:01 +0500

    chore: post django32 cleanup

--- commerce_coordinator/apps/demo_lms/urls.py
+++ commerce_coordinator/apps/demo_lms/urls.py
@@ -1,13 +1,14 @@
 """
 LMS app URLs, in a real service these would be callback handlers
 """
-from django.conf.urls import url
+
+from django.urls import path
 
 from commerce_coordinator.apps.demo_lms.views import demo_purchase_complete, test_view
 
 app_name = 'demo_lms'
 urlpatterns = [
     # DEMO: Test URL should be removed post proof-of-concept.
-    url(r'^test/', test_view, name='lms_test'),
-    url(r'^demo_purchase_complete/', demo_purchase_complete, name='lms_demo_purchase_complete'),
+    path('test/', test_view, name='lms_test'),
+    path('demo_purchase_complete/', demo_purchase_complete, name='lms_demo_purchase_complete'),
 ]
