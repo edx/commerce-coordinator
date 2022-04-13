dd968a14226e4d9ca8d66f1f09261036a9f3feb3
Diane Kaplan <dkaplan@edx.org>
2022-04-13 15:10:08 -0400

    fix: cleanup

--- commerce_coordinator/apps/orders/views.py
+++ commerce_coordinator/apps/orders/views.py
@@ -1,36 +1,24 @@
 """
 Views for the orders app
 """
-
+import logging
 from django.http import JsonResponse
-from .clients import EcommerceApiClient
 
-def get_user_orders__ecommerce(_):
+from .clients import EcommerceApiClient
+logger = logging.getLogger(__name__)
+
+def get_user_orders__ecommerce(request):
     """
     To start: get orders from ecommerce for hardcode
     """
+    username = request.GET['username']
+    page = request.GET['page']
+    page_size = request.GET['page_size']
+    params = {username, page, page_size}
 
-    # @@TODO: Once these are working, move them to the right place
-    ECOMMERCE_BASE_URL = 'http://localhost:18130'
-    ECOMMERCE_API_BASE_URL = ECOMMERCE_BASE_URL + '/api/v2'
-
-    # hardcode to get working, then figure out how to pass them inn
-    username = 'edx'
-    one = 1
-
-    params= {
-      username,
-      one,
-      one,
-    }
-
-    url_to_hit = f'{ECOMMERCE_API_BASE_URL}/orders/?page={one}&page_size={one}&username={username}'
+    logger.info(f'DKTEST: get_user_orders__ecommerce called with params: {params}.')
 
     ecommerce_api_client = EcommerceApiClient()
-    ecommerce_response = ecommerce_api_client.get_orders(username)
-
-    # print('DKTEST ecommerce_response: ', ecommerce_response)
-
-    # requests.get(url_to_hit, params=params) # ValueError at /orders/test/, too many values to unpack (expected 2)
+    ecommerce_response = ecommerce_api_client.get_orders(params)
 
     return JsonResponse(ecommerce_response)
