a27b09a4faadc1ba3c279a1cf017d4cfe2d10376
Diane Kaplan <dkaplan@edx.org>
2022-04-13 17:21:56 -0400

    fix: pull variables from query string

--- commerce_coordinator/apps/orders/views.py
+++ commerce_coordinator/apps/orders/views.py
@@ -5,16 +5,16 @@ Views for the orders app
 from django.http import JsonResponse
 from .clients import EcommerceApiClient
 
-def get_user_orders__ecommerce(_):
+def get_user_orders__ecommerce(request):
     """
     To start: get orders from ecommerce for hardcode
     """
-
-    # hardcode to get working, then figure out how to pass them inn
-    username = 'edx'
+    username = request.GET['username']
+    page = request.GET['page']
+    page_size = request.GET['page_size']
+    params = {username, page, page_size}
 
     ecommerce_api_client = EcommerceApiClient()
-    ecommerce_response = ecommerce_api_client.get_orders(username)
-
+    ecommerce_response = ecommerce_api_client.get_orders(params)
 
     return JsonResponse(ecommerce_response)
