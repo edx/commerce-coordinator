a77876ba039d27eef972bc9aa476c3b76a01046c
Phillip Shiu <pshiu@edx.org>
2022-04-13 12:38:08 -0400

    feat: add EcommerceApiClient

---
+++ commerce_coordinator/apps/orders/clients.py
@@ -0,0 +1,66 @@
+"""
+API clients for services that manage orders.
+"""
+import logging
+import requests
+
+from django.conf import settings
+from edx_rest_api_client.client import OAuthAPIClient
+
+logger = logging.getLogger(__name__)
+
+class BaseEdxOAuthClient:
+    """
+    API client for calls to the other edX services.
+    """
+
+    def __init__(self):
+        self.client = OAuthAPIClient(
+            settings.SOCIAL_AUTH_EDX_OAUTH2_URL_ROOT.strip('/'),
+            self.oauth2_client_id,
+            self.oauth2_client_secret,
+            timeout=(
+                settings.REQUEST_CONNECT_TIMEOUT_SECONDS,
+                settings.REQUEST_READ_TIMEOUT_SECONDS
+            )
+        )
+
+    @property
+    def oauth2_client_id(self):
+        return settings.BACKEND_SERVICE_EDX_OAUTH2_KEY
+
+    @property
+    def oauth2_client_secret(self):
+        return settings.BACKEND_SERVICE_EDX_OAUTH2_SECRET
+
+
+class EcommerceApiClient(BaseEdxOAuthClient):
+    """
+    API client for calls to the edX Ecommerce service.
+    """
+    api_base_url = settings.ECOMMERCE_URL + '/api/v2/'
+
+    def get_orders(self, username):
+        """
+        Call ecommerce API overview endpoint for data about an order.
+
+        Arguments:
+            username: restrict to orders by this username
+        Returns:
+            dict: Dictionary represention of JSON returned from API
+
+        example response:
+        {
+            # TODO: Fill in.
+        }
+        """
+        try:
+            query_params = {'username': username}
+            endpoint = self.api_base_url + 'orders/'
+            response = self.client.get(endpoint, params=query_params)
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.HTTPError as exc:
+            logger.exception(exc)
+            raise
+
--- provision-commerce-coordinator.sh
+++ provision-commerce-coordinator.sh
@@ -1,30 +1,9 @@
 name="commerce-coordinator"
 port="8140"
 
-docker-compose up -d --build
-
-# Install requirements
-# Can be skipped right now because we're using the --build flag on docker-compose. This will need to be changed once we move to devstack.
-
-# Wait for MySQL
-echo "Waiting for MySQL"
-until docker exec -i commerce-coordinator.db mysql -u root -se "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'root')" &> /dev/null
-do
-  printf "."
-  sleep 1
-done
-sleep 5
-
-# Create the database
-docker exec -i commerce-coordinator.db mysql -u root -se "CREATE DATABASE commerce-coordinator;"
-
-# Run migrations
-echo -e "${GREEN}Running migrations for ${name}...${NC}"
-docker exec -t commerce-coordinator.app bash -c "cd /edx/app/${name}/ && make migrate"
-
 # Create superuser
 echo -e "${GREEN}Creating super-user for ${name}...${NC}"
-docker exec -t commerce-coordinator.app bash -c "echo 'from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(\"edx\", \"edx@example.com\", \"edx\") if not User.objects.filter(username=\"edx\").exists() else None' | python /edx/app/${name}/manage.py shell"
+bash -c "echo 'from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(\"edx\", \"edx@example.com\", \"edx\") if not User.objects.filter(username=\"edx\").exists() else None' | python manage.py shell"
 
 # Provision IDA User in LMS
 echo -e "${GREEN}Provisioning ${name}_worker in LMS...${NC}"
@@ -34,5 +13,3 @@ docker exec -t edx.devstack.lms  bash -c "source /edx/app/edxapp/edxapp_env && p
 docker exec -t edx.devstack.lms  bash -c "source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker create_dot_application --grant-type authorization-code --skip-authorization --redirect-uris 'http://localhost:${port}/complete/edx-oauth2/' --client-id '${name}-sso-key' --client-secret '${name}-sso-secret' --scopes 'user_id' ${name}-sso ${name}_worker"
 docker exec -t edx.devstack.lms bash -c "source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker create_dot_application --grant-type client-credentials --client-id '${name}-backend-service-key' --client-secret '${name}-backend-service-secret' ${name}-backend-service ${name}_worker"
 
-# Restart enterprise.catalog app and worker containers
-docker-compose restart app
