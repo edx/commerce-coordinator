1000860df687dccdb4d37882f19730e3de88a3f5
Diane Kaplan <dkaplan@edx.org>
2022-04-14 15:43:08 -0400

    fix: update url path and name

--- commerce_coordinator/apps/orders/urls.py
+++ commerce_coordinator/apps/orders/urls.py
@@ -8,5 +8,5 @@ from commerce_coordinator.apps.orders.views import get_user_orders__ecommerce
 
 app_name = 'orders'
 urlpatterns = [
-    path('ecommerce/test/', get_user_orders__ecommerce, name='orders_test'),
+    path('ecommerce/', get_user_orders__ecommerce, name='orders_ecommerce'),
 ]
