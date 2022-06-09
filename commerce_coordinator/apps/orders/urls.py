6b5f610d6085bde374feacdb361f56d36a228b5e
Phillip Shiu <pshiu@edx.org>
2022-06-09 14:52:09 -0400

    feat: create redirect route for retrieving order history from ecommerce

--- commerce_coordinator/apps/orders/urls.py
+++ commerce_coordinator/apps/orders/urls.py
@@ -4,9 +4,9 @@ Orders app URLS
 
 from django.urls import path
 
-from commerce_coordinator.apps.orders.views import get_user_orders__ecommerce
+from commerce_coordinator.apps.orders.views import redirect_user_orders__ecommerce
 
 app_name = 'orders'
 urlpatterns = [
-    path('order_history/', get_user_orders__ecommerce, name='order_history'),
+    path('order_history/', redirect_user_orders__ecommerce, name='order_history'),
 ]
--- commerce_coordinator/apps/orders/views.py
+++ commerce_coordinator/apps/orders/views.py
@@ -3,11 +3,25 @@ Views for the orders app
 """
 import logging
 
+from django.conf import settings
 from django.http import JsonResponse
+from django.shortcuts import redirect
 
 from .clients import EcommerceApiClient
 
 logger = logging.getLogger(__name__)
+
+
+def redirect_user_orders__ecommerce(request):
+    """
+    Redirects user to ecommerce API for getting orders.
+    """
+    api_url = str(settings.ECOMMERCE_URL) + '/api/v2/orders/'
+
+    # The query string is get_full_path without the leading path.
+    query_string = request.get_full_path().replace(request.path, "", 1)
+
+    return redirect(api_url + query_string)
 
 
 def get_user_orders__ecommerce(request):
