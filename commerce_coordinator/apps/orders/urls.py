d52f8931fbc5491b9b8da53c4477587e0f833bba
Diane Kaplan <dkaplan@edx.org>
2022-07-20 14:11:31 -0400

    fix: update order history page to use new filter code

--- commerce_coordinator/apps/orders/urls.py
+++ commerce_coordinator/apps/orders/urls.py
@@ -4,12 +4,10 @@ Orders app URLS
 
 from django.urls import include, path
 
-from commerce_coordinator.apps.orders.views import EcommerceUserOrdersView, UserOrdersView
+from commerce_coordinator.apps.orders.views import UserOrdersView
 
 app_name = 'orders'
 urlpatterns = [
-    path('order_history/', EcommerceUserOrdersView.as_view(), name='order_history'),
-    # path('order_history/', UserOrdersView.as_view(), name='order_history'),
+    path('order_history/', UserOrdersView.as_view(), name='order_history'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('get_order_data/', UserOrdersView.as_view(), name='get_order_data'),
 ]
--- commerce_coordinator/apps/orders/views.py
+++ commerce_coordinator/apps/orders/views.py
@@ -9,33 +9,9 @@ from rest_framework.response import Response
 from rest_framework.throttling import UserRateThrottle
 from rest_framework.views import APIView
 
-from .clients import EcommerceApiClient
 from .filters import OrderDataRequested
 
 logger = logging.getLogger(__name__)
-
-
-class EcommerceUserOrdersView(APIView):
-    """Get the order history for the authenticated user."""
-    permission_classes = [LoginRedirectIfUnauthenticated]
-    throttle_classes = [UserRateThrottle]
-
-    def get(self, request):
-        """Return paginated response of user's order history."""
-
-        # deny global queries
-        if not request.user.username:
-            raise PermissionDenied(detail="Could not detect username.")
-
-        # build parameters
-        page = request.query_params.get("page")
-        page_size = request.query_params.get("page_size")
-        params = {'username': request.user.username, "page": page, "page_size": page_size}
-
-        # repeat what ecommerce returns
-        ecommerce_api_client = EcommerceApiClient()
-        ecommerce_response = ecommerce_api_client.get_orders(params)
-        return Response(ecommerce_response)
 
 
 class UserOrdersView(APIView):
@@ -50,4 +26,4 @@ class UserOrdersView(APIView):
         if not request.user.username:
             raise PermissionDenied(detail="Could not detect username.")
         order_data = OrderDataRequested.run_filter(request)
-        return Response({"order_data": order_data})
+        return Response(order_data[0])
