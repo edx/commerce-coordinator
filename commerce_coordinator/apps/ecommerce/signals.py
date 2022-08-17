2e84fece9ee9d70b23af21d1dc9042b6424eabf9
Phillip Shiu <pshiu@edx.org>
2022-08-17 16:21:47 -0400

    fix: remove unnecessary logging statements

--- commerce_coordinator/apps/ecommerce/signals.py
+++ commerce_coordinator/apps/ecommerce/signals.py
@@ -1,10 +1,6 @@
 """
 Ecommerce signals and receivers.
 """
-import logging
-
 from commerce_coordinator.apps.core.signal_helpers import CoordinatorSignal
-
-logger = logging.getLogger(__name__)
 
 enrollment_code_redemption_requested_signal = CoordinatorSignal()
