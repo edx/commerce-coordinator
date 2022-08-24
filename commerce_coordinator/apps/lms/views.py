6311ea696d0f1cb881c76ade99ecd4aec2d9c2ed
julianajlk <julianajlk@gmail.com>
2022-08-24 19:52:30 -0400

    feat: Add EnrollmentView in LMS app

--- commerce_coordinator/apps/lms/views.py
+++ commerce_coordinator/apps/lms/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+"""
+Views for the LMS app
+"""
 
-# Create your views here.
+import logging
+
+from edx_rest_framework_extensions.permissions import LoginRedirectIfUnauthenticated
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
+from rest_framework.throttling import UserRateThrottle
+from rest_framework.views import APIView
+
+logger = logging.getLogger(__name__)
+
+
+class EnrollmentView(APIView):
+    """
+    API for LMS enrollment.
+    """
+    permission_classes = [LoginRedirectIfUnauthenticated]
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    throttle_classes = [UserRateThrottle]
+
+    logger.info('LMS app views')
