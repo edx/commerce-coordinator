05638c98cd6818a822546c04367a2e5e1e1a7d29
julianajlk <julianajlk@gmail.com>
2022-08-24 20:57:37 -0400

    fix: Titan app signals.py correct import order

--- commerce_coordinator/apps/titan/signals.py
+++ commerce_coordinator/apps/titan/signals.py
@@ -4,9 +4,7 @@ Titan app signals and receivers.
 import logging
 
 from commerce_coordinator.apps.core.signal_helpers import coordinator_receiver
-from commerce_coordinator.apps.titan.tasks import (
-    enrollment_code_redemption_requested_create_order_task,
-)
+from commerce_coordinator.apps.titan.tasks import enrollment_code_redemption_requested_create_order_task
 
 logger = logging.getLogger(__name__)
 
