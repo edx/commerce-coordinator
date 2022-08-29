4f5b5fc75f0cab88610523f642086955439fa725
Phillip Shiu <pshiu@edx.org>
2022-08-29 07:56:19 -0400

    feat: add debug logging for celery tasks

--- commerce_coordinator/celery_app.py
+++ commerce_coordinator/celery_app.py
@@ -4,16 +4,25 @@ Celery app needed to configure using Django settings, and make Celery tasks avai
 https://docs.celeryproject.org/en/stable/django/first-steps-with-django.html
 """
 
+import logging
 import os
 
-from celery import Celery
+import celery
+import django.conf
 
 # Set the default configuration module, if one is not aleady defined.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commerce_coordinator.settings.local')
 
-app = Celery('commerce-coordinator')
+app = celery.Celery('commerce-coordinator')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps. Any Celery tasks located in a tasks.py file under a Django
 # app in INSTALLED_APPS should automatically be found.
 app.autodiscover_tasks()
+
+# Dive deeper into calls to log debug messages in tasks if Django logging level is also set to debug.
+@celery.signals.after_setup_task_logger.connect
+def on_after_setup_task_logger(**kwargs):
+    if django.conf.settings.DEBUG:
+        logger = kwargs["logger"]
+        logger.setLevel(logging.DEBUG)
