79edd8498e670938957923eef14bc625c95dd03b
Hammad Ahmad Waqas <hammadahmadwaqas1@gmail.com>
2023-03-02 19:30:58 +0500

    feat: Added order detail endpoint (#61)

--- commerce_coordinator/apps/demo_lms/signals.py
+++ commerce_coordinator/apps/demo_lms/signals.py
@@ -3,7 +3,7 @@ Demo LMS app signals and receivers.
 """
 import logging
 
-from commerce_coordinator.apps.core.signal_helpers import CoordinatorSignal, coordinator_receiver
+from commerce_coordinator.apps.core.signal_helpers import CoordinatorSignal, log_receiver
 from commerce_coordinator.apps.demo_lms.tasks import (
     demo_order_complete_send_confirmation_email_task,
     demo_order_complete_send_enroll_in_course_task,
@@ -17,11 +17,11 @@ logger = logging.getLogger(__name__)
 #############################################################
 
 
-@coordinator_receiver(logger)
+@log_receiver(logger)
 def test_receiver(**kwargs):
     """
     Basic signal receiver to test out connecting signals and handlers via config.
-    Due to the coordinator_receiver decorator, this is an example of what we'll emit to the logs:
+    Due to the log_receiver decorator, this is an example of what we'll emit to the logs:
     INFO 2394 [commerce_coordinator.apps.demo_lms.signals] test_receiver CALLED with sender '{sender}' and {kwargs}.
     """
     # This test can be modified accordingly to include more uses.
@@ -32,7 +32,7 @@ purchase_complete_signal = CoordinatorSignal()
 enroll_learner_signal = CoordinatorSignal()
 
 
-@coordinator_receiver(logger)
+@log_receiver(logger)
 def demo_purchase_complete_order_history(**kwargs):
     """
     This signal receiver would typically be in a separate app for just the Order History service, but is here for
@@ -41,7 +41,7 @@ def demo_purchase_complete_order_history(**kwargs):
     demo_order_complete_send_order_history_task.delay(kwargs['order_results'])
 
 
-@coordinator_receiver(logger)
+@log_receiver(logger)
 def demo_purchase_complete_send_confirmation_email(**kwargs):
     """
     This signal receiver would typically be in a separate app for just the email service, but is here for
@@ -51,7 +51,7 @@ def demo_purchase_complete_send_confirmation_email(**kwargs):
     demo_order_complete_send_confirmation_email_task.delay(kwargs['order_results'])
 
 
-@coordinator_receiver(logger)
+@log_receiver(logger)
 def demo_purchase_complete_enroll_in_course(**kwargs):
     """
     This signal receiver is one that legitimately belongs in LMS, it first off an enrollment event for each purchased
@@ -63,7 +63,7 @@ def demo_purchase_complete_enroll_in_course(**kwargs):
         enroll_learner_signal.send_robust('demo_purchase_complete', user_id=user_id, product_id=product_id)
 
 
-@coordinator_receiver(logger)
+@log_receiver(logger)
 def demo_enroll_learner_in_course(**kwargs):
     """
     This signal receiver is one that legitimately belongs in LMS, it would kick off a Celery task to LMS to enroll a
--- commerce_coordinator/apps/ecommerce/signals.py
+++ commerce_coordinator/apps/ecommerce/signals.py
@@ -4,3 +4,4 @@ Ecommerce signals and receivers.
 from commerce_coordinator.apps.core.signal_helpers import CoordinatorSignal
 
 enrollment_code_redemption_requested_signal = CoordinatorSignal()
+order_created_signal = CoordinatorSignal()
