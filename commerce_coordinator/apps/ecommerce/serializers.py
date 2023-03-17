825a430e6993f15e79d28067184ab28eb361cf84
Hammad Ahmad Waqas <hammadahmadwaqas1@gmail.com>
2023-03-17 14:39:36 +0500

    feat: added methods add_item and order_complete in TitanClient. (#67)

--- commerce_coordinator/apps/ecommerce/serializers.py
+++ commerce_coordinator/apps/ecommerce/serializers.py
@@ -6,9 +6,11 @@ class OrderCreatedSignalInputSerializer(serializers.Serializer):  # pylint: disa
     """
     Serializer for order_created_signal input validation.
     """
-    coupon_code = serializers.CharField(allow_null=True)
-    edx_lms_user_id = serializers.IntegerField(allow_null=False)
-    email = serializers.EmailField(allow_null=False)
     product_sku = serializers.ListField(
         child=serializers.CharField(), allow_empty=False
     )
+    edx_lms_user_id = serializers.IntegerField(allow_null=False)
+    email = serializers.EmailField(allow_null=False)
+    first_name = serializers.CharField(allow_null=False)
+    last_name = serializers.CharField(allow_null=False)
+    coupon_code = serializers.CharField(allow_null=True)
