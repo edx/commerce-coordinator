76f13fe3446e532537efec4294c35a8da3a6121e
Phillip Shiu <pshiu@edx.org>
2023-03-19 22:15:23 -0400

    refactor: urljoin_directory() helper for combining urls

---
+++ commerce_coordinator/apps/core/tests/test_clients.py
@@ -0,0 +1,28 @@
+"""Test core.clients."""
+
+import ddt
+from django.test import TestCase
+
+from commerce_coordinator.apps.core.clients import Client
+
+
+@ddt.ddt
+class ClientTests(TestCase):
+    """Tests of the Client class."""
+
+    @ddt.data(
+        ("http://localhost:18130/directory", "/subdirectory"),
+        ("http://localhost:18130/directory", "/subdirectory/"),
+        ("http://localhost:18130/directory", "subdirectory"),
+        ("http://localhost:18130/directory", "subdirectory/"),
+        ("http://localhost:18130/directory/", "/subdirectory"),
+        ("http://localhost:18130/directory/", "/subdirectory/"),
+        ("http://localhost:18130/directory/", "subdirectory"),
+        ("http://localhost:18130/directory/", "subdirectory/"),
+
+    )
+    @ddt.unpack
+    def test_urljoin_directory_trailing_slashes(self, base, suffix):
+        output = Client().urljoin_directory(base, suffix)
+        expected = "http://localhost:18130/directory/subdirectory"
+        self.assertRegex(output, expected + r"/?")
