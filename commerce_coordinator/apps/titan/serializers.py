7e7351e3741cf5e3ff520bd19a4253c832802f5b
Phillip Shiu <pshiu@edx.org>
2023-04-05 09:43:58 -0400

    fix: use UnixDateTimeField with OrderFulfillViewInputSerializer

--- commerce_coordinator/apps/titan/serializers.py
+++ commerce_coordinator/apps/titan/serializers.py
@@ -1,5 +1,5 @@
 """Serializers for Titan service"""
-from rest_framework import serializers
+from commerce_coordinator.apps.core import serializers
 
 
 class OrderFulfillViewInputSerializer(serializers.Serializer):  # pylint: disable=abstract-method
@@ -8,7 +8,7 @@ class OrderFulfillViewInputSerializer(serializers.Serializer):  # pylint: disabl
     """
     course_id = serializers.CharField(allow_null=False)
     course_mode = serializers.CharField(allow_null=False)
-    date_placed = serializers.DateTimeField(allow_null=False)
+    date_placed = serializers.UnixDateTimeField(allow_null=False)
     edx_lms_user_id = serializers.IntegerField(allow_null=False)
     email_opt_in = serializers.BooleanField(allow_null=False)
     order_number = serializers.UUIDField(allow_null=False)
