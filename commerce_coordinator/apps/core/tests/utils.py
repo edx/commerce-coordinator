cb408e97ba6af85a32fd2414de33109d6bba82ba
Phillip Shiu <pshiu@edx.org>
2023-05-01 16:19:24 -0400

    test: make some assertJSONClientResponse args optional
    Skip check for expected_request on GET requests, which do not have a
    request.body.
    
    Make expected_headers optional in case there are no headers to check.
    
    Move expected_headers and mock_method into method arguments to allow for
    more customization.

--- commerce_coordinator/apps/core/tests/utils.py
+++ commerce_coordinator/apps/core/tests/utils.py
@@ -80,11 +80,9 @@ class CoordinatorClientTestCase(TestCase):
     Note: There is no class named CoordinatorClient. This is a utility class.
     '''
 
-    mock_method = 'POST'
-    expected_headers = None
-
     @responses.activate
-    def assertJSONClientResponse(self, uut, input_kwargs, expected_request,
+    def assertJSONClientResponse(self, *, uut, input_kwargs, expected_request,
+                                 expected_headers=None, mock_method='POST',
                                  mock_url, mock_response, expected_output):
         '''
         Checks uut produces expected_request and expected_output given
@@ -93,39 +91,58 @@ class CoordinatorClientTestCase(TestCase):
         Mocks any calls by requests to self.mock_url. Returns
         mock_response for those calls as JSON.
 
-        Checks headers match self.expected_headers.
+        Optionally, checks headers match self.expected_headers.
 
         Args:
             uut (callable): Unit under test. Calls an external API using the
                 requests library.
             input_kwargs (dict): kwargs to provide uut.
             expected_request (dict): Expected request of uut to external API
-                given input_kwargs. Will be converted to JSON.
+                given input_kwargs. POST requests will be converted to JSON.
+            expected_headers (dict): Expected headers of uut to external API.
             mock_url (str): URL of external API to mock.
             mock_response (dict): Mock response external API should provide uut
                 given expected_request. Will be converted to JSON.
             expected_output (object): Expected return value of uut given
                 mock_response.
         '''
+        is_get = mock_method == 'GET'
+
+        # Use matcher for query params for GET requests:
+        if is_get:
+            expected_match = [
+                responses.matchers.query_param_matcher(expected_request)
+            ]
+        else:
+            expected_match = []
+
         # Prepare external API's mock response:
         responses.add(
-            method=self.mock_method,
+            method=mock_method,
             url=mock_url,
             json=mock_response,
+            match=expected_match,
         )
 
         # Get client output & request it built for external API:
         output = uut(**input_kwargs)
         request = responses.calls[-1].request
 
+        # Convert the request into a dictionary:
+        if is_get:
+            request_dict = request.params
+        else:
+            request_dict = json.loads(request.body)
+
         # Perform checks:
-        self.assertGreaterEqual(
-            request.headers.items(),
-            self.expected_headers.items(),
-            'Check external API called with expected headers'
-        )
+        if expected_headers:
+            self.assertGreaterEqual(
+                request.headers.items(),
+                expected_headers.items(),
+                'Check external API called with expected headers'
+            )
         self.assertEqual(
-            json.loads(request.body),
+            request_dict,
             expected_request,
             'Check external API called with expected input'
         )
