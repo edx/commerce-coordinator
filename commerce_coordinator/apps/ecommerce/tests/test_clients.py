244fb55102f028e0b9c7e6459ae6a287239b40f8
Phillip Shiu <pshiu@edx.org>
2023-05-02 07:50:21 -0400

    test: add CoordinatorOAuthClientTestCase
    Similar to CoordinatorClientTestCase, but also mocks the call to get the
    JWT token.
    
    Also refactor Ecommerce's old test case to
    CoordinatorOAuthClientTestCase.

--- commerce_coordinator/apps/ecommerce/tests/test_clients.py
+++ commerce_coordinator/apps/ecommerce/tests/test_clients.py
@@ -3,9 +3,9 @@ Tests for the ecommerce app API clients.
 """
 import logging
 
-from django.test import TestCase
-from mock import patch
+from django.test import override_settings
 
+from commerce_coordinator.apps.core.tests.utils import CoordinatorOAuthClientTestCase
 from commerce_coordinator.apps.ecommerce.clients import EcommerceAPIClient
 from commerce_coordinator.apps.frontend_app_ecommerce.tests import (
     ECOMMERCE_REQUEST_EXPECTED_RESPONSE,
@@ -14,17 +14,33 @@ from commerce_coordinator.apps.frontend_app_ecommerce.tests import (
 
 logger = logging.getLogger(__name__)
 
+TEST_ECOMMERCE_URL = 'https://testserver.com'
 
-class EcommerceClientTests(TestCase):
-    """
-    Verify endpoint availability for order retrieval endpoint(s)
-    """
 
-    @patch('commerce_coordinator.apps.ecommerce.clients.EcommerceAPIClient.get_orders')
-    def test_ecommerce_api_client(self, mock_response):
-        """We can call the EcommerceAPIClient successfully."""
-        mock_response.return_value = ECOMMERCE_REQUEST_EXPECTED_RESPONSE
+@override_settings(
+    ECOMMERCE_URL=TEST_ECOMMERCE_URL,
+    BACKEND_SERVICE_EDX_OAUTH2_PROVIDER_URL='https://testserver.com/auth'
+)
+class EcommerceAPIClientTests(CoordinatorOAuthClientTestCase):
+    """EcommerceAPIClient tests."""
 
-        ecommerce_api_client = EcommerceAPIClient()
-        ecommerce_response = ecommerce_api_client.get_orders(ORDER_HISTORY_GET_PARAMETERS)
-        self.assertEqual(ECOMMERCE_REQUEST_EXPECTED_RESPONSE, ecommerce_response)
+    def setUp(self):
+        self.client = EcommerceAPIClient()
+
+    def test_order_create_success(self):
+        url = TEST_ECOMMERCE_URL + '/api/v2/orders'
+        self.assertJSONClientResponse(
+            uut=self.client.get_orders,
+            input_kwargs={
+                'query_params': ORDER_HISTORY_GET_PARAMETERS
+            },
+            expected_request={
+                'username': 'TestUser',
+                'page': 1,
+                'page_size': 20,
+            },
+            mock_method='GET',
+            mock_url=url,
+            mock_response=ECOMMERCE_REQUEST_EXPECTED_RESPONSE,
+            expected_output=ECOMMERCE_REQUEST_EXPECTED_RESPONSE,
+        )
