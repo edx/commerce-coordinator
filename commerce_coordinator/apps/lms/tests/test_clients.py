2d071351214f2a8cbf38f185ac453852812479ef
Phillip Shiu <pshiu@edx.org>
2023-05-04 09:09:14 -0400

    test: add LMSAPIClientTests.test_order_create_failure

--- commerce_coordinator/apps/lms/tests/test_clients.py
+++ commerce_coordinator/apps/lms/tests/test_clients.py
@@ -1,10 +1,11 @@
-"""
+'''
 Tests for the lms app API clients.
-"""
+'''
 import logging
 
 from django.test import TestCase, override_settings
 from mock import patch
+from requests.exceptions import HTTPError
 
 from commerce_coordinator.apps.core.clients import urljoin_directory
 from commerce_coordinator.apps.core.tests.utils import CoordinatorOAuthClientTestCase
@@ -24,20 +25,37 @@ TEST_LMS_URL_ROOT = 'https://testserver.com'
     BACKEND_SERVICE_EDX_OAUTH2_PROVIDER_URL='https://testserver.com/auth'
 )
 class LMSAPIClientTests(CoordinatorOAuthClientTestCase):
-    """LMSAPIClient tests."""
+    '''LMSAPIClient tests.'''
+
+    url = urljoin_directory(TEST_LMS_URL_ROOT, '/api/enrollment/v1/enrollment')
 
     def setUp(self):
         self.client = LMSAPIClient()
 
     def test_order_create_success(self):
-        url = urljoin_directory(TEST_LMS_URL_ROOT, '/api/enrollment/v1/enrollment')
+        '''Check EXAMPLE_FULFILLMENT_*_PAYLOAD generates correct request and response.'''
         self.assertJSONClientResponse(
             uut=self.client.enroll_user_in_course,
             input_kwargs={
                 'enrollment_data': EXAMPLE_FULFILLMENT_REQUEST_PAYLOAD,
             },
             expected_request=EXAMPLE_FULFILLMENT_REQUEST_PAYLOAD,
-            mock_url=url,
+            mock_url=self.url,
             mock_response=EXAMPLE_FULFILLMENT_RESPONSE_PAYLOAD,
             expected_output=EXAMPLE_FULFILLMENT_RESPONSE_PAYLOAD,
         )
+
+    def test_order_create_failure(self):
+        '''Check empty request and mock 400 generates exception.'''
+        with self.assertRaises(HTTPError):
+            self.assertJSONClientResponse(
+                uut=self.client.enroll_user_in_course,
+                input_kwargs={
+                    'enrollment_data': {},
+                },
+                expected_request={},
+                mock_url=self.url,
+                mock_response={},
+                mock_status=400,
+                expected_output={},
+            )
