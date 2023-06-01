c2c4ed2174de9beb75b0ff677aa254c593973241
Hammad Ahmad Waqas <hammadahmadwaqas1@gmail.com>
2023-06-01 18:06:23 +0500

    feat: implemented payment_processed_save_task. (#80)

--- commerce_coordinator/apps/titan/tests/test_tasks.py
+++ commerce_coordinator/apps/titan/tests/test_tasks.py
@@ -3,8 +3,9 @@
 from django.test import TestCase
 from mock.mock import patch
 
-from commerce_coordinator.apps.titan.tasks import order_created_save_task
+from commerce_coordinator.apps.titan.tasks import order_created_save_task, payment_processed_save_task
 
+from ...core.constants import PaymentState
 from .test_clients import ORDER_CREATE_DATA, ORDER_UUID, TitanClientMock
 
 
@@ -36,3 +37,27 @@ class TestOrderTasks(TestCase):
 
         # values however should be the value expected as a return from complete_order
         self.assertEqual(list(values), [{'attributes': {'uuid': ORDER_UUID}}])
+
+
+class TestPaymentTasks(TestCase):
+    """Titan payment tests."""
+
+    @patch('commerce_coordinator.apps.titan.clients.TitanAPIClient.update_payment', new_callable=TitanClientMock)
+    def test_payment_processed_save_task(self, mock_update_payment):
+        """ Ensure that the `payment_processed_save_task` invokes create update_payment as expected.
+
+        Args:
+            mock_update_payment (TitanClientMock): Titan Client Mock for its mock_update_payment command.
+        """
+        payment_update_params = {
+            'payment_number': '12345',
+            'payment_state': PaymentState.COMPLETED.value,
+            'response_code': 'g7h52545gavgatTh'
+         }
+        payment_processed_save_task.apply(
+            kwargs=payment_update_params
+        ).get()
+
+        mock_update_payment.assert_called_with(
+            **payment_update_params
+        )
