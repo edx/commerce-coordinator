01c91be51391bf9863a52170f6064a393321c86c
hammadahmadwaqas <hammadahmadwaqas1@gmail.com>
2023-06-16 16:05:41 +0500

    fix: incorporated feedback.

--- commerce_coordinator/apps/frontend_app_payment/exceptions.py
+++ commerce_coordinator/apps/frontend_app_payment/exceptions.py
@@ -6,9 +6,3 @@ class InvalidOrderPayment(APIException):
     status_code = 409
     default_detail = 'Your requested payment does not belong to this payment'
     default_code = 'invalid_order_payment'
-
-
-class UnexpectedPayment(APIException):
-    status_code = 500
-    default_detail = 'Received Unexpected payment data'
-    default_code = 'unexpected_payment_details'
