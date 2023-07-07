47cc502b8bab522ef1767a981a029e33a17125e5
Hammad Ahmad Waqas <hammadahmadwaqas1@gmail.com>
2023-07-07 17:59:36 +0500

    feat: Added PaymentProcessView and PaymentProcessInputSerializer (#94)
    * feat: Added PaymentProcessView and PaymentProcessInputSerializer
    
    * fix: updated skus format from list of 'skus' to comma seperated 'skus'

--- commerce_coordinator/apps/frontend_app_payment/tests/test_filters.py
+++ commerce_coordinator/apps/frontend_app_payment/tests/test_filters.py
@@ -6,7 +6,7 @@ from unittest.mock import patch
 from django.test import override_settings
 
 from commerce_coordinator.apps.core.constants import PaymentState
-from commerce_coordinator.apps.frontend_app_payment.filters import DraftPaymentRequested
+from commerce_coordinator.apps.frontend_app_payment.filters import DraftPaymentRequested, PaymentProcessingRequested
 from commerce_coordinator.apps.titan.tests.test_clients import ORDER_UUID
 
 
@@ -43,3 +43,40 @@ class TestDraftPaymentRequestedFilter(TestCase):
         }
         payment_details = DraftPaymentRequested.run_filter(**filter_params)
         self.assertEqual(mock_payment.get('payment_data'), payment_details)
+
+
+class TestPaymentProcessingRequestedFilter(TestCase):
+    """ A pytest Test Case for then `PaymentProcessingRequested` """
+
+    @override_settings(
+        OPEN_EDX_FILTERS_CONFIG={
+            "org.edx.coordinator.frontend_app_payment.payment.processing.requested.v1": {
+                "fail_silently": False,
+                "pipeline": [
+                    'commerce_coordinator.apps.titan.pipeline.GetTitanPayment',
+                ]
+            },
+        },
+    )
+    @patch('commerce_coordinator.apps.titan.pipeline.GetTitanPayment.run_filter')
+    def test_filter_when_payment_exist_in_titan(self, mock_pipeline):
+        """
+        Test when Payment exists in Titan system.
+        """
+
+        mock_payment = {
+            'payment_number': 'test-payment-number',
+            'order_uuid': ORDER_UUID,
+            'key_id': 'test-intent-id',
+            'state': PaymentState.PROCESSING.value
+        }
+        mock_pipeline.return_value = mock_payment
+        filter_params = {
+            'order_uuid': ORDER_UUID,
+            'payment_number': 'test-payment-number',
+            'payment_intent_id': 'test-intent-id',
+            'skus': ['test-sku'],
+        }
+        payment_details = PaymentProcessingRequested.run_filter(**filter_params)
+        expected_payment = {**mock_payment, **filter_params}
+        self.assertEqual(expected_payment, payment_details)
