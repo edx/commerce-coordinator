edd9d43bb5b3a1807793c5d547fd6a40210d41d2
Glenn R. Martin <gmartin@2u.com>
2023-09-13 07:01:07 -0400

    chore: pydocs, helpers and some basic cleanup
    more to come.

--- commerce_coordinator/apps/commercetools/management/commands/import_discovery.py
+++ commerce_coordinator/apps/commercetools/management/commands/import_discovery.py
@@ -1,3 +1,4 @@
+import inspect
 import json
 from io import BytesIO
 
@@ -6,10 +7,8 @@ import urllib3
 from commercetools import CommercetoolsError
 from commercetools.importapi import Client
 from commercetools.importapi.models import AssetDimensions, AttributeConstraintEnum, \
-    AttributeDefinition, AttributeType, \
-    Image, LocalizedString, \
-    MoneyType, PriceDraftImport, ProductDraftImport, \
-    ProductTypeImport, \
+    AttributeDefinition, AttributeType, Image, LocalizedString, \
+    MoneyType, PriceDraftImport, ProductDraftImport, ProductTypeImport, \
     ProductTypeKeyReference, ProductVariantDraftImport, TextAttribute, TypedMoney
 # noinspection PyProtectedMember
 from commercetools.importapi.models.importcontainers import ImportContainerDraft, ImportResourceType
@@ -37,7 +36,7 @@ from PIL import Image as PILImage
 DISCO_MAX_PER_PAGE = 10
 
 
-def ls(string_dict) -> LocalizedString:
+def ls(string_dict) -> LocalizedString:  # forced return typehint/coercion intentional to avoid bad IDE warnings
     """ Make a LocalizedString that doesn't freak out type checking, assign en to en-US as well. """
     if 'en' in string_dict:
         # Keys are CASE sensitive. String matching the pattern ^[a-z]{2}(-[A-Z]{2})?$ representing an IETF language tag
@@ -50,8 +49,37 @@ class Command(BaseCommand):
     help = "Import Discovery Courses to CommerceTools"
     course_product_type_key = 'edx-course_verified'
 
+    # Helpers
+    @staticmethod
+    def handle_commercetools_error(error: CommercetoolsError, exit_code: int):
+        """
+        Standardize CommerceTools error while revealing "why" (for example what API values were invalid.
+
+        Args:
+            error: CommercetoolsError Object
+            exit_code: Shell Exit code (anything over 0 is a fatal error)
+        """
+
+        print(f"*** ERROR at ln{inspect.currentframe().f_back.f_lineno} => {error}")
+        print(f"*** DETAILS => {error.errors}\n\n")
+
+        if exit_code > 0:
+            exit(exit_code)
+
     @staticmethod
     def fetch_from_discovery(index, last_sort, alter_query=None, **options):
+        """
+        Invoke Discoery's ES _search API and return it's result
+
+        Args:
+            index: Which ES index to use
+            last_sort: The last pagination sort value from the final item in the last call
+            alter_query: A mutator that can modify the query object before it is sent
+            **options: Options (CLI input options) from the main method (`Command.handle`)
+
+        Returns: urllib3.BaseHTTPResponse
+
+        """
         query_template = """
         {
             "query": {"match_all": {}},
@@ -92,19 +120,31 @@ class Command(BaseCommand):
         }
 
     @staticmethod
-    def get_courses(**options):
+    def get_courses(last_sort_index=None, **options):
+        # TODO: Determine if done to support pagnation, were using the search_after method (as its stable)
+        #    https://www.elastic.co/guide/en/elasticsearch/reference/current/paginate-search-results.html#search-after
 
         return {
-            'courses': Command.fetch_from_discovery('course', None, **options).json()['hits']['hits'],
+            'courses': Command.fetch_from_discovery('course', last_sort_index, **options).json()['hits']['hits'],
             'done': True,
             'page': 1
         }
 
     @staticmethod
     def get_image_dimensions(url):
+        """
+        Get the Dimensions of an image based on URL
+
+        Args:
+            url: HTTP/S URL to Resource
+
+        Returns (AssetDimensions): Dimensions of Image for CommerceTools
+
+        """
         im = PILImage.open(BytesIO(urllib3.request('GET', url).data))
         return AssetDimensions(w=im.width, h=im.height)
 
+    # Django Overrides
     def add_arguments(self, parser):
         parser.add_argument("discovery_host", nargs="?", type=str, default="http://127.0.0.1:9200")
         pass
@@ -183,9 +223,7 @@ class Command(BaseCommand):
                 ])
             )
         except CommercetoolsError as err:
-            print(err)
-            print(err.errors)
-            exit(1)
+            self.handle_commercetools_error(err, 1)
 
         course_key_ref = ProductTypeKeyReference(key=self.course_product_type_key)
 
@@ -237,6 +275,4 @@ class Command(BaseCommand):
                 ProductDraftImportRequest(resources=product_drafts)
             )
         except CommercetoolsError as err:
-            print(err)
-            print(err.errors)
-            exit(2)
+            self.handle_commercetools_error(err, 2)
