5d186f980163b7ec8fce575b396d3e6d20770028
Glenn R. Martin <gmartin@2u.com>
2023-10-11 11:17:26 -0400

    fixed example.

--- commerce_coordinator/apps/commercetools/tests/conftest.py
+++ commerce_coordinator/apps/commercetools/tests/conftest.py
@@ -1,6 +1,3 @@
-import typing
-
-import pytest
 import requests_mock
 from commercetools import Client
 from commercetools.testing import BackendRepository
@@ -8,13 +5,16 @@ from commercetools.testing import BackendRepository
 from commerce_coordinator.apps.commercetools.clients import CommercetoolsAPIClient
 
 
-def commercetools_client_tuple(
-) -> typing.Tuple[requests_mock.Mocker, BackendRepository, CommercetoolsAPIClient]:
-    requests_mock.mock.case_sensitive = False
-    with requests_mock.Mocker(real_http=False, case_sensitive=False) as m:
-        repo = BackendRepository()
-        repo.register(m)
-        return m, repo, CommercetoolsAPIClient(Client(
+class APITestingSet:
+    mocker: requests_mock.Mocker
+    backend_repo: BackendRepository
+    client: CommercetoolsAPIClient
+
+    def __init__(self, mocker: requests_mock.Mocker, repo: BackendRepository):
+        self.mocker = mocker
+        self.backend_repo = repo
+        mocker.start()
+        self.client = CommercetoolsAPIClient(Client(
             project_key="unittest",
             client_id="client-id",
             client_secret="client-secret",
@@ -23,20 +23,12 @@ def commercetools_client_tuple(
             token_url="https://auth.europe-west1.gcp.commercetools.com/oauth/token",
         ))
 
+    def __del__(self):
+        self.mocker.stop()
 
-# @pytest.fixture(scope="function")
-# def commercetools_client_tuple(
-# ) -> typing.Generator[typing.Tuple[requests_mock.Mocker, BackendRepository, CommercetoolsAPIClient], None, None]:
-#     requests_mock.mock.case_sensitive = False
-#     with requests_mock.Mocker(real_http=False, case_sensitive=False) as m:
-#         repo = BackendRepository()
-#         repo.register(m)
-#         yield m, repo, CommercetoolsAPIClient(Client(
-#             project_key="unittest",
-#             client_id="client-id",
-#             client_secret="client-secret",
-#             scope=[],
-#             url="https://api.europe-west1.gcp.commercetools.com",
-#             token_url="https://auth.europe-west1.gcp.commercetools.com/oauth/token",
-#         ))
-#         repo.internal = None
+
+def commercetools_client_set() -> APITestingSet:
+    m = requests_mock.Mocker(real_http=True, case_sensitive=False)
+    repo = BackendRepository()
+    repo.register(m)
+    return APITestingSet(m, repo)
--- commerce_coordinator/apps/commercetools/tests/test_clients.py
+++ commerce_coordinator/apps/commercetools/tests/test_clients.py
@@ -1,58 +1,21 @@
 """ Commercetools API Client(s) Testing """
-import unittest
-from typing import Optional
 
-import ddt
-import pytest
 from commercetools.platform.models import Type
-from commercetools.testing import BackendRepository
 from django.test import TestCase
-import time
-import uuid
-
-import pytest
-from requests_mock.adapter import Adapter
-
-from commercetools import CommercetoolsError
-from commercetools.client import Client
-from commercetools.platform import models
 
 from commerce_coordinator.apps.commercetools.catalog_info.foundational_types import TwoUCustomTypes
-from commerce_coordinator.apps.commercetools.clients import CommercetoolsAPIClient
-from commerce_coordinator.apps.core.clients import urljoin_directory
-from conftest import commercetools_client_tuple
-from commercetools.contrib.pytest import ct_platform_client, commercetools_api
-
-
-# def test_xxxx(commercetools_client_tuple):
-#     (mocker, backing_store, api_client) = commercetools_client_tuple
-#     draft = TwoUCustomTypes.CUSTOMER_TYPE_DRAFT
-#     ret_val = api_client.ensure_custom_type_exists(draft)
-#     assert isinstance(ret_val, Type)
-#     assert ret_val.key == draft.key
+from conftest import APITestingSet, commercetools_client_set
 
 
 class ClientTests(TestCase):
-    backing_store: Optional[BackendRepository] = None
-    api_client: Optional[CommercetoolsAPIClient] = None
+    client_set: APITestingSet
 
     def setUp(self) -> None:
         super().setUp()
-        print("GRM: xxxxx")
-        (mocker, backing_store, api_client) = commercetools_client_tuple()
-        self.backing_store = backing_store
-        self.api_client = api_client
-        self.mocker = mocker
-        breakpoint()
-
-    def tearDown(self) -> None:
-        self.backing_store = None
-        self.mocker = None
-        self.api_client = None
-        super().tearDown()
+        self.client_set = commercetools_client_set()
 
     def test_ensure_custom_type_exists(self):
         draft = TwoUCustomTypes.CUSTOMER_TYPE_DRAFT
-        ret_val = self.api_client.ensure_custom_type_exists(draft)
+        ret_val = self.client_set.client.ensure_custom_type_exists(draft)
         assert isinstance(ret_val, Type)
         assert ret_val.key == draft.key
