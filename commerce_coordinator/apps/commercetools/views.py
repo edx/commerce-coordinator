8adbdb67fa028974671eb469489df214df7261c2
Glenn R. Martin <gmartin@2u.com>
2023-12-08 14:40:30 -0500

    chore: adding test coverage and commenting out some failures for later
    The view should be tested both IRL and in Unit Testing in SONIC-117.

--- commerce_coordinator/apps/commercetools/views.py
+++ commerce_coordinator/apps/commercetools/views.py
@@ -7,19 +7,11 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from commerce_coordinator.apps.core.models import User
+from commerce_coordinator.apps.commercetools.serializers import OrderFulfillViewInputSerializer
 from commerce_coordinator.apps.core.signal_helpers import format_signal_results
-from commerce_coordinator.apps.ecommerce.serializers import OrderFulfillViewInputSerializer
 from commerce_coordinator.apps.ecommerce.signals import fulfill_order_placed_signal
 
-from .catalog_info.constants import DEFAULT_ORDER_EXPANSION
-from .catalog_info.edx_utils import (
-    get_edx_items,
-    get_edx_lms_user_id,
-    get_edx_lms_user_name,
-    get_edx_product_course_run_key,
-    is_edx_lms_order
-)
+from .catalog_info.edx_utils import get_edx_items, get_edx_lms_user_id, get_edx_product_course_run_key, is_edx_lms_order
 from .clients import CommercetoolsAPIClient
 from .serializers import OrderFulfillMessageInputSerializer
 
@@ -35,15 +27,22 @@ class OrderFulfillView(APIView):
             **request.data
         }
 
-        logger.debug('[OrderFulfillView] Message received from commercetools with details: %s', input_data)
+        logger.debug('[CT-OrderFulfillView] Message received from commercetools with details: %s', input_data)
 
         message_details = OrderFulfillMessageInputSerializer(data=input_data)
         message_details.is_valid(raise_exception=True)
 
         client = CommercetoolsAPIClient()
-        order_id = message_details.detail.orderId
+        order_id = message_details.data['order_id']
         order = client.get_order_by_id(order_id)
         customer = client.get_customer_by_id(order.customer_id)
+
+        if not (customer and order and is_edx_lms_order(order)):
+            logger.debug('[CT-OrderFulfillView] order %s is not an edX order', order_id)
+
+            return Response(status=status.HTTP_200_OK)
+
+        logger.debug('[CT-OrderFulfillView] processing edX order %s', order_id)
 
         lms_user_id = get_edx_lms_user_id(customer)
 
@@ -51,34 +50,27 @@ class OrderFulfillView(APIView):
             'email_opt_in': True,  # ?? Where?
             'order_number': order.order_number or order.id,
             'provider_id': None,
-            'user': get_edx_lms_user_name(customer),
+            'edx_lms_user_id': lms_user_id,
             'course_mode': 'verified',
             'date_placed': (order.completed_at or order.last_modified_at).timestamp(),
         }
 
-        if customer and order and is_edx_lms_order(order):
-            logger.debug('[OrderFulfillView] processing edX order %s', order_id)
-            items = get_edx_items(order)
+        for item in get_edx_items(order):
+            logger.debug('[CT-OrderFulfillView] processing edX order %s, line item %s', order_id, item.variant.sku)
 
-            for item in items:
-                serializer = OrderFulfillViewInputSerializer(data={
-                    **default_params,
-                    'course_id': get_edx_product_course_run_key(item),  # likely not correct
-                })
+            serializer = OrderFulfillViewInputSerializer(data={
+                **default_params,
+                'course_id': get_edx_product_course_run_key(item),  # likely not correct
+            })
 
-                if serializer.is_valid(raise_exception=True):
-                    payload = serializer.validated_data
+            if serializer.is_valid(raise_exception=True):
+                payload = serializer.validated_data
 
-                    payload['edx_lms_user_id'] = lms_user_id
-                    payload.pop('user')
+                results = fulfill_order_placed_signal.send_robust(
+                    sender=self.__class__,
+                    **payload
+                )
 
-                    results = fulfill_order_placed_signal.send_robust(
-                        sender=self.__class__,
-                        **payload
-                    )
-
-                    return Response(format_signal_results(results), status=status.HTTP_200_OK)
-        else:
-            logger.debug('[OrderFulfillView] order %s is not an edX order', order_id)
+                return Response(format_signal_results(results), status=status.HTTP_200_OK)
 
         return Response(status=status.HTTP_200_OK)
