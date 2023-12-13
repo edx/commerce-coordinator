ce8e5354d0e5a15c1af7800e5b01d95bd38966ae
Glenn R. Martin <gmartin@2u.com>
2023-12-13 09:57:12 -0500

    fix: monkeypath failed in pipeline using old mocker format

--- commerce_coordinator/apps/lms/tests/test_views.py
+++ commerce_coordinator/apps/lms/tests/test_views.py
@@ -2,7 +2,7 @@
 Tests for the LMS (edx-platform) views.
 """
 import ddt
-from commercetools.platform.models import ProductProjectionPagedSearchResponse as CTProductProjectionPagedSearchResponse
+import requests_mock
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -10,57 +10,46 @@ from mock import patch
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from commerce_coordinator.apps.commercetools.clients import CommercetoolsAPIClient
-from commerce_coordinator.apps.commercetools.tests.conftest import MonkeyPatch, gen_variant_search_result
-from commerce_coordinator.apps.core.constants import ORDER_HISTORY_PER_SYSTEM_REQ_LIMIT
+from commerce_coordinator.apps.commercetools.tests.conftest import (
+    APITestingSet,
+    gen_variant_search_result
+)
 
 User = get_user_model()
 
-
 @ddt.ddt
 class GetActiveManagementSystemTests(APITestCase):
+
     url = reverse('lms:payment_page_redirect')
 
-
-    def setupVariantSearch(self):
-        self.variant_search_results = gen_variant_search_result()
-        pass
-
-    def get_product_variant_by_course_run(self):
-        # noinspection PyUnusedLocal
-        # pylint: disable=unused-argument # needed for kwargs
-        def _get_product_variant_by_course_run(
-            _, cr_name: str
-        ) -> CTProductProjectionPagedSearchResponse:
-            return self.variant_search_results
-        # pylint: enable=unused-argument # needed for kwargs
-
-        return _get_product_variant_by_course_run
-
-    def setUp(self):
+    def setUp(self) -> None:
         super().setUp()
-        self.setupVariantSearch()
-        MonkeyPatch.monkey(
-            CommercetoolsAPIClient,
-            {
-                '__init__': lambda _: None,
-                'get_product_variant_by_course_run': self.get_product_variant_by_course_run()
-            }
-        )
+        self.client_set = APITestingSet.new_instance()
 
-    def tearDown(self):
+    def tearDownCTApiClient(self) -> None:
+        # force deconstructor call or some test get flaky
+        del self.client_set
         super().tearDown()
-        if MonkeyPatch.is_monkey(CommercetoolsAPIClient):
-            MonkeyPatch.unmonkey(CommercetoolsAPIClient)
 
     @patch('commerce_coordinator.apps.rollout.pipeline.is_redirect_to_commercetools_enabled_for_user')
     def test_run_rollout_pipeline_redirect_to_commercetools_enabled(self, is_redirect_mock):
-        breakpoint()
-        is_redirect_mock.return_value = True
-        response = self.client.get(self.url,
-                                   {'sku': ['sku1'], 'course_run_key': 'course-v1:MichiganX+InjuryPreventionX+1T2021'})
-        # self.assertIn(ret_variant, response.url)
-        self.assertTrue(response.headers['Location'].startswith(settings.COMMERCETOOLS_FRONTEND_URL))
+        base_url = self.client_set.get_base_url_from_client()
+
+        # Because the base mocker can't do param binding, we have to intercept.
+        with requests_mock.Mocker(real_http=True, case_sensitive=False) as mocker:
+            mocker.get(
+                f"{base_url}product-projections/search?"
+                f"markMatchingVariants=False"
+                f"&filter=variants.sku%3A%22course-v1%3AMichiganX%2BInjuryPreventionX%2B1T2021%22",
+                json=gen_variant_search_result().serialize()
+            )
+
+            ret_variant = self.client_set.client.get_product_variant_by_course_run('course-v1:MichiganX+InjuryPreventionX+1T2021')
+            breakpoint()
+            is_redirect_mock.return_value = True
+            response = self.client.get(self.url, {'sku': ['sku1'], 'course_run_key': 'course-v1:MichiganX+InjuryPreventionX+1T2021'})
+            # self.assertIn(ret_variant, response.url)
+            self.assertTrue(response.headers['Location'].startswith(settings.COMMERCETOOLS_FRONTEND_URL))
 
     # @patch('commerce_coordinator.apps.rollout.pipeline.is_redirect_to_commercetools_enabled_for_user')
     # def test_run_filter_only_sku_available(self, is_redirect_mock):
@@ -69,7 +58,6 @@ class GetActiveManagementSystemTests(APITestCase):
     #     is_redirect_mock.return_value = False
     #     response = self.client.get(self.url, {'sku': ['sku1']})
     #     self.assertTrue(response.headers['Location'].startswith(settings.FRONTEND_APP_PAYMENT_URL))
-
 
 
 @ddt.ddt
@@ -83,21 +71,14 @@ class PaymentPageRedirectViewTests(APITestCase):
     test_user_password = 'secret'
     url = reverse('lms:payment_page_redirect')
 
-    def setUp(self):
-        """Create test user before test starts."""
+    def setUp(self) -> None:
         super().setUp()
-        self.user = User.objects.create_user(
-            self.test_user_username,
-            self.test_user_email,
-            self.test_user_password,
-            # TODO: Remove is_staff=True
-            is_staff=True,
-        )
+        self.client_set = APITestingSet.new_instance()
 
-    def tearDown(self):
-        """Log out any user from client after test ends."""
+    def tearDown(self) -> None:
+        # force deconstructor call or some test get flaky
+        del self.client_set
         super().tearDown()
-        self.client.logout()
 
     def test_view_rejects_session_auth(self):
         """Check Session Auth Not Allowed."""
