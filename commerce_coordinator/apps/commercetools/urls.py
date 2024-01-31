69dcdcbbb899d4148b29d2fa208b630dccafc73d
Jade Olivier <jadeolivier95@gmail.com>
2024-01-31 14:32:42 +0200

    chore: Update commerce_coordinator/apps/commercetools/urls.py
    Co-authored-by: Glenn R. Martin <grmartin@users.noreply.github.com>

--- commerce_coordinator/apps/commercetools/urls.py
+++ commerce_coordinator/apps/commercetools/urls.py
@@ -9,6 +9,7 @@ from commerce_coordinator.apps.commercetools.views import OrderFulfillView, Orde
 app_name = 'commercetools'
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # EventBridge / CloudWatch Endpoints
     path('fulfill', OrderFulfillView.as_view(), name='fulfill'),
     path('sanctioned', OrderSanctionedView.as_view(), name='sanctioned'),
 ]
