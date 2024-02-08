287c1d3056b10a5308190829a6546fd52c5d90fa
Jade Olivier <jadeolivier95@gmail.com>
2024-02-08 15:43:42 +0200

    feat: Endpoint for 'sanctioned' order message (#162)
    * feat: add order confirmation email logic (#149) (#152)
    
    * fix: fix date_placed for enrollment call (#153)
    
    * feat: Added endpoint for sanctioned order message
    
    * chore: Update commerce_coordinator/apps/commercetools/urls.py
    
    ---------
    
    Co-authored-by: Mohammad Ahtasham ul Hassan <60315450+aht007@users.noreply.github.com>
    Co-authored-by: Glenn R. Martin <grmartin@users.noreply.github.com>
    Co-authored-by: Glenn R. Martin <gmartin@2u.com>

--- commerce_coordinator/apps/commercetools/urls.py
+++ commerce_coordinator/apps/commercetools/urls.py
@@ -4,10 +4,12 @@ commercetools app URLS
 
 from django.urls import include, path
 
-from commerce_coordinator.apps.commercetools.views import OrderFulfillView
+from commerce_coordinator.apps.commercetools.views import OrderFulfillView, OrderSanctionedView
 
 app_name = 'commercetools'
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # EventBridge / CloudWatch Endpoints
     path('fulfill', OrderFulfillView.as_view(), name='fulfill'),
+    path('sanctioned', OrderSanctionedView.as_view(), name='sanctioned'),
 ]
