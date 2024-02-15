28de7507b32077be54328466113ab098862b35ff
Glenn R. Martin <gmartin@2u.com>
2024-02-15 13:33:52 -0500

    fix: tests are now 100% as they were

--- commerce_coordinator/apps/commercetools/pipeline.py
+++ commerce_coordinator/apps/commercetools/pipeline.py
@@ -1,9 +1,9 @@
 """
 Commercetools filter pipelines
 """
-from abc import abstractmethod
 from logging import getLogger
 
+import attrs
 from commercetools import CommercetoolsError
 from openedx_filters import PipelineStep
 
@@ -19,54 +19,12 @@ from commerce_coordinator.apps.core.constants import PipelineCommand
 log = getLogger(__name__)
 
 
-class CommercetoolsPipelineStep(PipelineStep):
-    """ Commercetools Pipeline Step """
-
-    # noinspection PyMethodMayBeStatic
-    def return_command_for_error(self, _error: CommercetoolsError) -> PipelineCommand:
-        """
-        Return a PipelineCommand if we error
-        Args:
-            _error: The exception
-
-        Returns: CONTINUE or HALT
-
-        """
-        return PipelineCommand.CONTINUE
-
-    @abstractmethod
-    def run_safe_filter(self, **kwargs):
-        """
-        Abstract pipeline step runner.
-
-        Used to implement custom code that'll be executed by OpenEdxPublicFilter's pipeline runner.
-        It must be implemented by child classes.
-        """
-        log.warning(
-            "PipelineStep run_safe_filter method not implemented.\n"
-            "Child classes must implement this method with their custom code.\n"
-            "By design, the pipeline expects either of three (3) types of returns:\n"
-            "1. A dictionary with the arguments the method received. They can be modified in the process.\n"
-            "2. None. Returning this will stop the pipeline execution. The accumulated output until "
-            "this moment will be returned.\n"
-            "3. An object different from a dict. Returning this will stop the pipeline execution. "
-            "The accumulated output until this moment will be returned.\n"
-        )
-
-    def run_filter(self, **kwargs):
-        try:
-            return self.run_safe_filter(**kwargs)
-        except CommercetoolsError as err:
-            log.exception(f"Commercetools Error: {err}, {err.errors}", err)
-            return self.return_command_for_error(err).value
-
-
-class GetCommercetoolsOrders(CommercetoolsPipelineStep):
+class GetCommercetoolsOrders(PipelineStep):
     """
     Adds commercetools orders to the order data list.
     """
 
-    def run_safe_filter(self, params, order_data):  # pylint: disable=arguments-differ
+    def run_filter(self, params, order_data):  # pylint: disable=arguments-differ
         """
         Execute a filter with the signature specified.
         Arguments:
@@ -75,29 +33,34 @@ class GetCommercetoolsOrders(CommercetoolsPipelineStep):
         Returns:
         """
 
-        ct_api_client = CommercetoolsAPIClient()
-        ct_orders = ct_api_client.get_orders_for_customer(
-            edx_lms_user_id=params["edx_lms_user_id"],
-            limit=params["page_size"],
-            offset=params["page"] * params["page_size"]
-        )
+        try:
+            ct_api_client = CommercetoolsAPIClient()
+            ct_orders = ct_api_client.get_orders_for_customer(
+                edx_lms_user_id=params["edx_lms_user_id"],
+                limit=params["page_size"],
+                offset=params["page"] * params["page_size"]
+            )
 
-        converted_orders = [order_from_commercetools(x, ct_orders[1])
-                            for x in ct_orders[0].results]
+            # noinspection PyTypeChecker
+            converted_orders = [attrs.asdict(order_from_commercetools(x, ct_orders[1]))
+                                for x in ct_orders[0].results]
 
-        order_data.append(
-            ct_orders[0].rebuild(converted_orders)
-        )
+            order_data.append(
+                ct_orders[0].rebuild(converted_orders)
+            )
 
-        return {
-            "order_data": order_data
-        }
+            return {
+                "order_data": order_data
+            }
+        except CommercetoolsError as err:
+            log.exception(f"[{type(self).__name__}] Commercetools Error: {err}, {err.errors}", err)
+            return PipelineCommand.CONTINUE.value
 
 
-class FetchOrderDetails(CommercetoolsPipelineStep):
+class FetchOrderDetails(PipelineStep):
     """ Fetch the order Details and if we can, set the PaymentIntent """
 
-    def run_safe_filter(self, active_order_management_system, order_number):  # pylint: disable=arguments-differ
+    def run_filter(self, active_order_management_system, order_number):  # pylint: disable=arguments-differ
         """
         Execute a filter with the signature specified.
         Arguments:
@@ -107,21 +70,25 @@ class FetchOrderDetails(CommercetoolsPipelineStep):
         Returns:
         """
         if not active_order_management_system == COMMERCETOOLS_ORDER_MANAGEMENT_SYSTEM and \
-                active_order_management_system is not None:
+            active_order_management_system is not None:
             return PipelineCommand.CONTINUE.value
 
-        ct_api_client = CommercetoolsAPIClient()
-        ct_order = ct_api_client.get_order_by_id(order_id=order_number)
+        try:
+            ct_api_client = CommercetoolsAPIClient()
+            ct_order = ct_api_client.get_order_by_id(order_id=order_number)
 
-        ret_val = {
-            "order_data": ct_order
-        }
+            ret_val = {
+                "order_data": ct_order
+            }
 
-        for pr in ct_order.payment_info.payments:
-            pmt = pr.obj
-            if pmt.payment_status.interface_code == PAYMENT_STATUS_INTERFACE_CODE_SUCCEEDED \
-                and pmt.payment_method_info.payment_interface == EDX_STRIPE_PAYMENT_INTERFACE_NAME and \
+            for pr in ct_order.payment_info.payments:
+                pmt = pr.obj
+                if pmt.payment_status.interface_code == PAYMENT_STATUS_INTERFACE_CODE_SUCCEEDED \
+                    and pmt.payment_method_info.payment_interface == EDX_STRIPE_PAYMENT_INTERFACE_NAME and \
                     pmt.interface_id:
-                ret_val['payment_intent_id'] = pmt.interface_id
+                    ret_val['payment_intent_id'] = pmt.interface_id
 
-        return ret_val
+            return ret_val
+        except CommercetoolsError as err:
+            log.exception(f"[{type(self).__name__}] Commercetools Error: {err}, {err.errors}", err)
+            return PipelineCommand.CONTINUE.value
