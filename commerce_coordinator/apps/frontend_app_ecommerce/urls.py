6b64c97c232a16f7306770dea1db9ba98e9f9249
Glenn R. Martin <grmartin@users.noreply.github.com>
2024-03-05 04:51:08 -0500

    fix: Update commerce_coordinator/apps/frontend_app_ecommerce/urls.py
    Co-authored-by: Shafqat Farhan <shafqat.farhan@arbisoft.com>

--- commerce_coordinator/apps/frontend_app_ecommerce/urls.py
+++ commerce_coordinator/apps/frontend_app_ecommerce/urls.py
@@ -9,6 +9,6 @@ from commerce_coordinator.apps.frontend_app_ecommerce.views import RedirectRecei
 app_name = 'frontend_app_ecommerce'
 urlpatterns = [
     path('order_history/', UserOrdersView.as_view(), name='order_history'),
-    path('order_receipt', RedirectReceiptView.as_view(), name='order_receipt'),
+    path('order_receipt/', RedirectReceiptView.as_view(), name='order_receipt'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
