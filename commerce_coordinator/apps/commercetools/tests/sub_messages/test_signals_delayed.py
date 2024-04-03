4fd7027e39882015530eaa98b9e13582a13f3ae1
Glenn R. Martin <gmartin@2u.com>
2024-04-03 11:21:23 -0400

    fix: Signals testing for Subscription Messages

---
+++ commerce_coordinator/apps/commercetools/tests/sub_messages/test_signals_delayed.py
@@ -0,0 +1,96 @@
+""" Commercetools Subscription Message Signals """
+
+import logging
+from unittest.mock import patch
+
+from django.test import override_settings
+
+from commerce_coordinator.apps.commercetools.constants import SOURCE_SYSTEM
+from commerce_coordinator.apps.core.tests.utils import CoordinatorSignalReceiverTestCase, uuid4_str
+
+# Log using module name.
+logger = logging.getLogger(__name__)
+
+
+@override_settings(
+    CC_SIGNALS={
+        'commerce_coordinator.apps.core.tests.utils.example_signal': [
+            'commerce_coordinator.apps.commercetools.sub_messages.signals_delayed.fulfill_order_placed_message_signal',
+        ],
+    }
+)
+@patch('commerce_coordinator.apps.commercetools.sub_messages.signals_delayed.fulfill_order_placed_message_signal_task')
+class FulfillOrderPlacedMessageSignalTest(CoordinatorSignalReceiverTestCase):
+    """ Commercetools Fulfillment Order Placed Signal Tester"""
+    mock_parameters = {
+        'order_id': uuid4_str(),
+        'source_system': SOURCE_SYSTEM,
+    }
+
+    def test_correct_arguments_passed(self, mock_task):
+        _, logs = self.fire_signal()
+        logger.info('logs.output: %s', logs.output)
+        mock_task.delay.assert_called_once_with(**self.mock_parameters)
+
+    def test_correct_response_returned(self, mock_task):
+        mock_task.delay.return_value.id = 'bogus_task_id'
+        result, _ = self.fire_signal()
+        logger.info('result: %s', result)
+        self.assertEqual(result[0][1], 'bogus_task_id',
+                         'Check receiver result is same as return from task')
+
+
+@override_settings(
+    CC_SIGNALS={
+        'commerce_coordinator.apps.core.tests.utils.example_signal': [
+            'commerce_coordinator.apps.commercetools.sub_messages.signals_delayed'
+            '.fulfill_order_sanctioned_message_signal',
+        ],
+    }
+)
+@patch('commerce_coordinator.apps.commercetools.sub_messages.signals_delayed'
+       '.fulfill_order_sanctioned_message_signal_task')
+class SanctionedOrderPlacedMessageSignalTest(CoordinatorSignalReceiverTestCase):
+    """ Commercetools Sanctioned Order Placed Signal Tester"""
+    mock_parameters = {
+        'order_id': uuid4_str(),
+    }
+
+    def test_correct_arguments_passed(self, mock_task):
+        _, logs = self.fire_signal()
+        logger.info('logs.output: %s', logs.output)
+        mock_task.delay.assert_called_once_with(**self.mock_parameters)
+
+    def test_correct_response_returned(self, mock_task):
+        mock_task.delay.return_value.id = 'bogus_task_id'
+        result, _ = self.fire_signal()
+        logger.info('result: %s', result)
+        self.assertEqual(result[0][1], 'bogus_task_id',
+                         'Check receiver result is same as return from task')
+
+
+@override_settings(
+    CC_SIGNALS={
+        'commerce_coordinator.apps.core.tests.utils.example_signal': [
+            'commerce_coordinator.apps.commercetools.sub_messages.signals_delayed.fulfill_order_returned_signal',
+        ],
+    }
+)
+@patch('commerce_coordinator.apps.commercetools.sub_messages.signals_delayed.fulfill_order_returned_signal_task')
+class ReturnedOrderPlacedMessageSignalTest(CoordinatorSignalReceiverTestCase):
+    """ Commercetools Returned Order Placed Signal Tester"""
+    mock_parameters = {
+        'order_id': uuid4_str(),
+    }
+
+    def test_correct_arguments_passed(self, mock_task):
+        _, logs = self.fire_signal()
+        logger.info('logs.output: %s', logs.output)
+        mock_task.delay.assert_called_once_with(**self.mock_parameters)
+
+    def test_correct_response_returned(self, mock_task):
+        mock_task.delay.return_value.id = 'bogus_task_id'
+        result, _ = self.fire_signal()
+        logger.info('result: %s', result)
+        self.assertEqual(result[0][1], 'bogus_task_id',
+                         'Check receiver result is same as return from task')
--- commerce_coordinator/apps/commercetools/tests/sub_messages/test_tasks.py
+++ commerce_coordinator/apps/commercetools/tests/sub_messages/test_tasks.py
@@ -4,11 +4,15 @@ import logging
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 
-import ddt
 from edx_django_utils.cache import TieredCache
 
+from commerce_coordinator.apps.commercetools.catalog_info.constants import TwoUKeys
 from commerce_coordinator.apps.commercetools.constants import SOURCE_SYSTEM
-from commerce_coordinator.apps.commercetools.sub_messages.tasks import fulfill_order_placed_message_signal_task
+from commerce_coordinator.apps.commercetools.sub_messages.tasks import (
+    fulfill_order_placed_message_signal_task,
+    fulfill_order_returned_signal_task,
+    fulfill_order_sanctioned_message_signal_task
+)
 from commerce_coordinator.apps.commercetools.tests.mocks import (
     CTCustomerByIdMock,
     CTOrderByIdMock,
@@ -16,6 +20,16 @@ from commerce_coordinator.apps.commercetools.tests.mocks import (
 )
 from commerce_coordinator.apps.core.memcache import safe_key
 from commerce_coordinator.apps.core.tests.utils import uuid4_str
+
+# Log using module name.
+logger = logging.getLogger(__name__)
+
+# Define unit under test.
+# Note: if the UUT is part of the class as an ivar, it trims off arg0 as 'self' and
+#       claims too many args supplied
+fulfill_order_placed_uut = fulfill_order_placed_message_signal_task
+fulfill_order_sanctioned_uut = fulfill_order_sanctioned_message_signal_task
+fulfill_order_returned_uut = fulfill_order_returned_signal_task
 
 
 def gen_example_fulfill_payload():
@@ -50,16 +64,6 @@ class CommercetoolsAPIClientMock(MagicMock):
         self.expected_customer = self.customer_mock.return_value
 
 
-# Log using module name.
-logger = logging.getLogger(__name__)
-
-# Define unit under test.
-# Note: if the UUT is part of the class as an ivar, it trims off arg0 as 'self' and
-#       claims too many args supplied
-fulfill_order_placed_uut = fulfill_order_placed_message_signal_task
-
-
-@ddt.ddt
 @patch('commerce_coordinator.apps.commercetools.sub_messages.tasks.fulfill_order_placed_signal.send_robust',
        new_callable=SendRobustSignalMock)
 @patch('commerce_coordinator.apps.commercetools.sub_messages.tasks.CommercetoolsAPIClient',
@@ -106,3 +110,123 @@ class FulfillOrderPlacedMessageSignalTaskTests(TestCase):
         mock_values.order_mock.assert_called_once_with(mock_values.order_id)
         mock_values.customer_mock.assert_called_once_with(mock_values.customer_id)
         self.assertFalse(TieredCache.get_cached_response(mock_values.cache_key).is_found)
+
+
+@patch('commerce_coordinator.apps.commercetools.sub_messages.tasks.fulfill_order_placed_signal.send_robust',
+       new_callable=SendRobustSignalMock)
+@patch('commerce_coordinator.apps.commercetools.sub_messages.tasks.CommercetoolsAPIClient',
+       new_callable=CommercetoolsAPIClientMock)
+class OrderSanctionedMessageSignalTaskTests(TestCase):
+    """Tests for the fulfill_order_sanctioned_message_signal_task"""
+
+    @staticmethod
+    def unpack_for_uut(values):
+        """ Unpack the dictionary in the order required for the UUT """
+        return (
+            values['order_id'],
+        )
+
+    @staticmethod
+    def get_uut():
+        return fulfill_order_sanctioned_uut
+
+    def test_correct_arguments_passed(self, _ct_client_init: CommercetoolsAPIClientMock, _lms_signal):
+        """
+        Check calling uut with mock_parameters yields call to client with
+        expected_data.
+        """
+        mock_values = _ct_client_init.return_value
+        _ = self.get_uut()(*self.unpack_for_uut(mock_values.example_payload))
+
+        mock_values.order_mock.assert_called_once_with(mock_values.expected_order.id)
+        mock_values.customer_mock.assert_called_once_with(mock_values.expected_customer.id)
+
+    @patch('commerce_coordinator.apps.commercetools.sub_messages.tasks.is_edx_lms_order',
+           return_value=None)
+    def test_not_lms_order(self, _fn, _ct_client_init: CommercetoolsAPIClientMock, _lms_signal):
+        """
+        Check calling uut with mock_parameters yields call to client with
+        expected_data.
+        """
+        mock_values = _ct_client_init.return_value
+
+        ret_val = self.get_uut()(*self.unpack_for_uut(mock_values.example_payload))
+
+        self.assertTrue(ret_val)
+        mock_values.order_mock.assert_called_once_with(mock_values.order_id)
+        mock_values.customer_mock.assert_called_once_with(mock_values.customer_id)
+
+    @patch('commerce_coordinator.apps.commercetools.sub_messages.tasks.get_edx_order_workflow_state_key',
+           return_value=None)
+    def test_missing_order_workflow_state(self, _fn, _ct_client_init: CommercetoolsAPIClientMock, _lms_signal):
+        """
+        Check calling uut with mock_parameters yields call to client with
+        expected_data.
+        """
+        mock_values = _ct_client_init.return_value
+
+        ret_val = self.get_uut()(*self.unpack_for_uut(mock_values.example_payload))
+
+        self.assertIsNone(ret_val)
+        mock_values.order_mock.assert_called_once_with(mock_values.order_id)
+        mock_values.customer_mock.assert_called_once_with(mock_values.customer_id)
+
+    @patch('commerce_coordinator.apps.commercetools.sub_messages.tasks.get_edx_is_sanctioned',
+           return_value=False)
+    def test_order_not_sanctioned(self, _fn, _ct_client_init: CommercetoolsAPIClientMock, _lms_signal):
+        """
+        Check calling uut with mock_parameters yields call to client with
+        expected_data.
+        """
+        mock_values = _ct_client_init.return_value
+
+        ret_val = self.get_uut()(*self.unpack_for_uut(mock_values.example_payload))
+
+        self.assertIsNone(ret_val)
+        mock_values.order_mock.assert_called_once_with(mock_values.order_id)
+        mock_values.customer_mock.assert_called_once_with(mock_values.customer_id)
+
+
+@patch('commerce_coordinator.apps.commercetools.sub_messages.tasks.fulfill_order_placed_signal.send_robust',
+       new_callable=SendRobustSignalMock)
+@patch('commerce_coordinator.apps.commercetools.sub_messages.tasks.CommercetoolsAPIClient',
+       new_callable=CommercetoolsAPIClientMock)
+class OrderReturnedMessageSignalTaskTests(TestCase):
+    """Tests for the fulfill_order_returned_signal_task"""
+
+    @staticmethod
+    def unpack_for_uut(values):
+        """ Unpack the dictionary in the order required for the UUT """
+        return (
+            values['order_id'],
+        )
+
+    @staticmethod
+    def get_uut():
+        return fulfill_order_returned_uut
+
+    def test_correct_arguments_passed(self, _ct_client_init: CommercetoolsAPIClientMock, _lms_signal):
+        """
+        Check calling uut with mock_parameters yields call to client with
+        expected_data.
+        """
+        mock_values = _ct_client_init.return_value
+        _ = self.get_uut()(*self.unpack_for_uut(mock_values.example_payload))
+
+        mock_values.order_mock.assert_called_once_with(mock_values.expected_order.id)
+        mock_values.customer_mock.assert_called_once_with(mock_values.expected_customer.id)
+
+    @patch('commerce_coordinator.apps.commercetools.sub_messages.tasks.is_edx_lms_order',
+           return_value=False)
+    def test_not_lms_order(self, _fn, _ct_client_init: CommercetoolsAPIClientMock, _lms_signal):
+        """
+        Check calling uut with mock_parameters yields call to client with
+        expected_data.
+        """
+        mock_values = _ct_client_init.return_value
+
+        ret_val = self.get_uut()(*self.unpack_for_uut(mock_values.example_payload))
+
+        self.assertTrue(ret_val)
+        mock_values.order_mock.assert_called_once_with(mock_values.order_id)
+        mock_values.customer_mock.assert_called_once_with(mock_values.customer_id)
