0c490dedda1e7ea011830cfb4150e77b0d4ceb6d
Phillip Shiu <pshiu@edx.org>
2024-04-03 17:26:19 -0400

    fix: commerce-coordinator-backend-service, not commerce_coordinator-backend-service (same for -sso)
    (cherry picked from commit 22f93e4fdf244a0b78c105dc7ab73d77eac6c587)

--- local-provision-commerce-coordinator.sh
+++ local-provision-commerce-coordinator.sh
@@ -1,5 +1,7 @@
-name="commerce-coordinator"
+name="commerce_coordinator"
 port="8140"
+
+ida_name="commerce-coordinator"
 
 # Create superuser
 echo -e "${GREEN}Creating super-user for ${name}...${NC}"
@@ -10,17 +12,5 @@ echo -e "${GREEN}Provisioning ${name}_worker in LMS...${NC}"
 docker exec -t edx.devstack.lms  bash -c "source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker manage_user ${name}_worker ${name}_worker@example.com --staff --superuser"
 
 # Create the DOT applications - one for single sign-on and one for backend service IDA-to-IDA authentication.
-docker exec -t edx.devstack.lms  bash -c "source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker create_dot_application --grant-type authorization-code --skip-authorization --redirect-uris 'http://localhost:${port}/complete/edx-oauth2/' --client-id '${name}-sso-key' --client-secret '${name}-sso-secret' --scopes 'user_id' ${name}-sso ${name}_worker"
-docker exec -t edx.devstack.lms bash -c "source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker create_dot_application --grant-type client-credentials --client-id '${name}-backend-service-key' --client-secret '${name}-backend-service-secret' ${name}-backend-service ${name}_worker"
-
-
-# Titan setup
-name="titan"
-
-# Provision IDA User in LMS for Titan
-echo -e "${GREEN}Provisioning ${name}_worker in LMS...${NC}"
-docker exec -t edx.devstack.lms  bash -c "source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker manage_user ${name}_worker ${name}_worker@example.com --staff --superuser"
-
-# Create DOT application for backend service IDA-to-IDA authentication for Titan
-docker exec -t edx.devstack.lms bash -c "source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker create_dot_application --grant-type client-credentials --client-id '${name}-backend-service-key' --client-secret '${name}-backend-service-secret' ${name}-backend-service ${name}_worker"
-
+docker exec -t edx.devstack.lms  bash -c "source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker create_dot_application --grant-type authorization-code --skip-authorization --redirect-uris 'http://localhost:${port}/complete/edx-oauth2/' --client-id '${ida_name}-sso-key' --client-secret '${ida_name}-sso-secret' --scopes 'user_id' ${ida_name}-sso ${name}_worker"
+docker exec -t edx.devstack.lms bash -c "source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker create_dot_application --grant-type client-credentials --client-id '${ida_name}-backend-service-key' --client-secret '${ida_name}-backend-service-secret' ${ida_name}-backend-service ${name}_worker"
