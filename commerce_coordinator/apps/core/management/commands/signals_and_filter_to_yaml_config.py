e8c2de097c2f7766e0b435c17ab4f1abf8f3fbe7
Glenn R. Martin <gmartin@2u.com>
2024-04-18 14:46:37 -0400

    feat: YAML Config Generator for Signals and Filters

---
+++ commerce_coordinator/apps/core/management/commands/signals_and_filter_to_yaml_config.py
@@ -0,0 +1,47 @@
+import os
+import re
+
+import yaml
+from django.conf import settings
+from django.core.management import BaseCommand
+
+NL = os.linesep
+
+# taken from edx-internal config
+START_INDENT = "                "
+LIST_INDENT = "    "
+
+
+class Command(BaseCommand):
+    """Django Admin Command to generate YAML config for signals and filters."""
+
+    help = 'Generates YAML config for signals and filters'
+
+    @staticmethod
+    def to_yaml(obj) -> str:
+        """Convert object to YAML string"""
+        return yaml.dump(
+            obj,
+            default_flow_style=False,
+            line_break=NL
+        )
+
+    @staticmethod
+    def indent_like_edx_internal(string: str) -> str:
+        """Indent like edx-internal config YAML format"""
+        out = map(
+            lambda x: f"{START_INDENT}{x}",
+            map(
+                lambda x: f"{LIST_INDENT}{x}" if re.search(r"^\s*?- ", x) else x,
+                string.split(NL)
+            )
+        )
+
+        return NL.join(out)
+
+    def handle(self, *args, **options):
+        print(f"## Coordinator Signals YAML ############################################################################{NL}")
+        print(Command.indent_like_edx_internal(Command.to_yaml(settings.CC_SIGNALS)))
+        print(f"{NL}{NL}## Coordinator Filters YAML ####################################################################{NL}")
+        print(Command.indent_like_edx_internal(Command.to_yaml(settings.OPEN_EDX_FILTERS_CONFIG)))
+        print(f"{NL}{NL}## END  ########################################################################################{NL}")
