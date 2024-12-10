6d77e7a35aedd1a9ce8fa31748ff9fdc0e39f174
mubbsharanwar <mubbshar.anwar@arbisoft.com>
2024-12-10 14:18:49 +0500

    fix: handle paypal payment recipt handle paypal payment recipt url
    SONIC-784

--- commerce_coordinator/apps/paypal/apps.py
+++ commerce_coordinator/apps/paypal/apps.py
@@ -4,5 +4,5 @@ App configuration for the Commerce Coordinator paypal app.
 from django.apps import AppConfig
 
 
-class PayPalConfig(AppConfig):
+class PaypalConfig(AppConfig):
     name = 'commerce_coordinator.apps.paypal'
