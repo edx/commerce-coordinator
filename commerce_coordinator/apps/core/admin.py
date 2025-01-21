ea0461bd6b091ef29c087f9d2be76b6a802e8567
Mohammad Ahtasham ul Hassan <60315450+aht007@users.noreply.github.com>
2025-01-21 15:31:48 +0500

    Revert "temp: add lms_user_id to django admin (#322)"
    This reverts commit b67d6786a67fc996cf9a4a49eaf1becdb0f03c9c.

--- commerce_coordinator/apps/core/admin.py
+++ commerce_coordinator/apps/core/admin.py
@@ -12,7 +12,7 @@ class CustomUserAdmin(UserAdmin):
     """ Admin configuration for the custom User model. """
     list_display = ('username', 'email', 'full_name', 'first_name', 'last_name', 'is_staff', 'lms_user_id')
     fieldsets = (
-        (None, {'fields': ('username', 'password', 'lms_user_id')}),
+        (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('full_name', 'first_name', 'last_name', 'email')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
