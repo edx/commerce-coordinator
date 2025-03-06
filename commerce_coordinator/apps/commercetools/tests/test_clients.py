d635315395865a8359d807956d039fe1ff5b7ced
Muhammad Abdullah Waheed <42172960+abdullahwaheed@users.noreply.github.com>
2025-03-06 16:42:58 +0500

    refactor: changes on program feature PR suggestions (#357)
    * refactor: changes acording to PR suggestions
    
    * fix: unit tests and linting
    
    * feat: handled the scenario of re-return with same interation ID
    
    * fix: fixed unit tests
    
    * fix: segment event fix and handled refunded amount scenario

--- commerce_coordinator/apps/commercetools/tests/test_clients.py
+++ commerce_coordinator/apps/commercetools/tests/test_clients.py
@@ -8,7 +8,6 @@ from commercetools.platform.models import (
     Customer,
     CustomerDraft,
     CustomerPagedQueryResponse,
-    MoneyType,
     Order,
     OrderPagedQueryResponse,
     ReturnInfo,
@@ -17,7 +16,6 @@ from commercetools.platform.models import (
     TransactionState,
     TransactionType,
     Type,
-    TypedMoney,
     TypeDraft
 )
 from django.test import TestCase
@@ -490,10 +488,7 @@ class ClientTests(TestCase):
         # Mocked expected order recieved after CT SDK call to update the order
         mock_response_order = gen_order("mock_order_id")
         mock_payment = gen_payment_with_multiple_transactions(TransactionType.CHARGE, 4900, TransactionType.REFUND,
-                                                              TypedMoney(cent_amount=4900,
-                                                                         currency_code='USD',
-                                                                         type=MoneyType.CENT_PRECISION,
-                                                                         fraction_digits=2))
+                                                              4900)
         mock_response_order.version = "3"
         mock_response_return_item = gen_return_item("mock_return_item_id", ReturnPaymentState.REFUNDED)
         mock_response_return_info = ReturnInfo(items=[mock_response_return_item])
@@ -525,8 +520,9 @@ class ClientTests(TestCase):
                 mock_order.version,
                 [mock_response_return_item.line_item_id],
                 {mock_response_return_item.line_item_id: uuid4_str()},
+                {},
                 mock_payment.id,
-                10000
+                uuid4_str()
             )
             self.assertEqual(result.return_info[1].items[0].payment_state, ReturnPaymentState.REFUNDED)
 
@@ -1048,8 +1044,9 @@ class ClientUpdateReturnTests(TestCase):
                 order_version="2",
                 return_line_item_return_ids=["mock_return_item_id"],
                 return_line_entitlement_ids={'mock_return_item_id': 'mock_entitlement_id'},
+                refunded_line_item_refunds={},
                 payment_intent_id="1",
-                amount_in_cents=10000
+                interaction_id=uuid4_str()
             )
 
     def test_update_return_payment_state_no_payment(self):
@@ -1074,8 +1071,9 @@ class ClientUpdateReturnTests(TestCase):
                 order_version="2",
                 return_line_item_return_ids=["mock_return_item_id"],
                 return_line_entitlement_ids={'mock_return_item_id': 'mock_entitlement_id'},
-                payment_intent_id=None,
-                amount_in_cents=10000
+                refunded_line_item_refunds={},
+                payment_intent_id="1",
+                interaction_id=uuid4_str()
             )
 
     def test_get_product_by_program_id(self):
--- commerce_coordinator/apps/rollout/utils.py
+++ commerce_coordinator/apps/rollout/utils.py
@@ -31,13 +31,12 @@ def get_order_return_info_return_items(order: CTOrder) -> List[CTReturnItem]:
     return return_info_items
 
 
-def is_commercetools_line_item_already_refunded(
+def is_commercetools_line_item_already_created(
     order: CTOrder,
-    order_line_item_id: str,
-    return_info_return_items=None
+    order_line_item_id: str
 ) -> bool:
     """
-    Checks if a given commercetools line item has already been refunded.
+    Checks if a given commercetools line item has already been created.
     Args:
         order (CTOrder): The commercetools order object. If not provided, it will be fetched using the order object.
         order_line_item_id (str): The ID of the order line item to check.
@@ -45,11 +44,30 @@ def is_commercetools_line_item_already_refunded(
     Returns:
         bool: True if the line item has already been refunded, False otherwise.
     """
+    return_info_return_items = get_order_return_info_return_items(order)
+
+    return len(list(filter(lambda item: item.line_item_id == order_line_item_id, return_info_return_items))) >= 1
+
+
+def is_commercetools_line_item_already_refunded(
+    order: CTOrder,
+    order_line_item_id: str,
+    return_info_return_items=None
+) -> CTReturnItem:
+    """
+    Checks if a given commercetools line item has already been refunded.
+    Args:
+        order (CTOrder): The commercetools order object. If not provided, it will be fetched using the order object.
+        order_line_item_id (str): The ID of the order line item to check.
+        return_info_return_items (list): A list of return items containing information about returned line items.
+    Returns:
+        CTReturnItem: CTReturnItem if the line item has already been refunded, None otherwise.
+    """
     return_info_return_items = get_order_return_info_return_items(order) if not return_info_return_items else \
         return_info_return_items
     return next(
         (
-            item.line_item_id for item in return_info_return_items if item.line_item_id == order_line_item_id
+            item for item in return_info_return_items if item.line_item_id == order_line_item_id
             and item.payment_state == ReturnPaymentState.REFUNDED
         ), None
     )
