1cf444fe99f8216581dbb98e3f4c3378cd217183
Muhammad Abdullah Waheed <abdullah.waheed@arbisoft.com>
2025-03-12 14:10:10 +0500

    refactor: moved custom item creation util to a separate file

--- commerce_coordinator/apps/commercetools/management/commands/_utils.py
+++ commerce_coordinator/apps/commercetools/management/commands/_utils.py
@@ -1,5 +1,10 @@
 import sys
+import json
+
 from typing import Literal, Union
+
+from commercetools import CommercetoolsError
+from commercetools.platform.models import TypeAddFieldDefinitionAction
 
 
 def yn(question: str, default: Union[Literal['y', 'n']] = "n"):
@@ -19,3 +24,27 @@ def yn(question: str, default: Union[Literal['y', 'n']] = "n"):
             return resps[choice]
         else:
             sys.stdout.write("Please choose y or n.\n")
+
+
+def handle_custom_item_creation(self, custom_type):
+    try:
+        type_key = custom_type.key
+        ret = self.ct_api_client.base_client.types.get_by_key(type_key)
+        data = ret.serialize()
+        print(f"{custom_type.resource_type_ids[0]} custom type with field {type_key} already exists: \n{
+            json.dumps(data)}")
+
+        existing_fields = [field.get('name') for field in data.get('fieldDefinitions', [])]
+
+        for field in custom_type.field_definitions:
+            if field.name not in existing_fields:
+                update_action = TypeAddFieldDefinitionAction(field_definition=field)
+                ret = self.ct_api_client.base_client.types.update_by_id(
+                    data.get('id'), data.get('version'), actions=[update_action]
+                )
+                print(f"Created field: {field.name} for custom type: {type_key} {json.dumps(ret.serialize())}")
+
+    except CommercetoolsError as ex:
+        ret = self.ct_api_client.base_client.types.create(custom_type)
+        print(f"Created {custom_type.resource_type_ids[0]} custom type with field {type_key}: {
+            json.dumps(ret.serialize())}")
