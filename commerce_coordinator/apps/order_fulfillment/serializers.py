cba982ecfb9ee3499a335647e856dfc18e929347
Muhammad Noyan Aziz <naziz1@2u.com>
2025-04-28 15:22:07 +0500

    feat: SONIC-998 Order fulfillment webhook (#393)
    * feat: add fulfill-order webhook in cc
    
    * refactor: remove logging code
    
    * refactor: remove extra imports
    
    * refactor: remove extra imports
    
    * refactor: blank spaces
    
    * refactor: blank spaces
    
    * fix: checks
    
    * feat: add unit tests
    
    * fix: handle review comments
    
    * fix: add/update tests
    
    * fix: improve code
    
    ---------
    
    Co-authored-by: Muhammad Noyan  Aziz <noyan.aziz@A006-01474.local>
    Co-authored-by: Syed Sajjad  Hussain Shah <ssajjad@2u.com>

---
+++ commerce_coordinator/apps/order_fulfillment/serializers.py
@@ -0,0 +1,18 @@
+"""Serializers for LMS (edx-platform) service"""
+from rest_framework import serializers
+
+from commerce_coordinator.apps.core.serializers import CoordinatorSerializer
+
+
+class FulfillOrderWebhookSerializer(CoordinatorSerializer):
+    """
+    Serializer for Fulfill Order Webhook input validation.
+    """
+    fulfillment_type = serializers.CharField()
+    is_fulfilled = serializers.BooleanField()
+    entitlement_uuid = serializers.CharField(required=False)
+    order_id = serializers.CharField()
+    order_version = serializers.CharField()
+    line_item_id = serializers.CharField()
+    item_quantity = serializers.IntegerField()
+    line_item_state_id = serializers.CharField()
--- commerce_coordinator/apps/order_fulfillment/views.py
+++ commerce_coordinator/apps/order_fulfillment/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+"""
+Views for the order fulfillment app
+"""
+import logging
 
-# Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from commerce_coordinator.apps.core.views import SingleInvocationAPIView
+from commerce_coordinator.apps.lms.clients import FulfillmentType
+from commerce_coordinator.apps.lms.signals import fulfillment_completed_update_ct_line_item_signal
+from commerce_coordinator.apps.order_fulfillment.serializers import FulfillOrderWebhookSerializer
+from commerce_coordinator.apps.order_fulfillment.webhook_utils.webhook_authentication import (
+    HMACSignatureWebhookAuthentication
+)
+
+logger = logging.getLogger(__name__)
+
+
+class FulfillmentResponseWebhookView(SingleInvocationAPIView):
+    """
+    Endpoint for Order Fulfillment webhook Response. This endpoint receives fulfillment
+    response from fulfillment providers and updates CT order object with response data.
+    """
+    http_method_names = ['post']
+    authentication_classes = [HMACSignatureWebhookAuthentication]
+    permission_classes = [AllowAny]
+
+    @csrf_exempt
+    def post(self, request):
+        """Webhook entry point for order fulfillment response."""
+        tag = type(self).__name__
+
+        input_data = {
+            **request.data
+        }
+
+        logger.info(f'[CT-{tag}] Message received from order-fulfillment with details: {input_data}')
+
+        validator = FulfillOrderWebhookSerializer(data=input_data)
+        validator.is_valid(raise_exception=True)
+        validated_data = validator.validated_data
+
+        fulfillment_type = validated_data.get('fulfillment_type')
+        if fulfillment_type == FulfillmentType.ENTITLEMENT.value:
+            entitlement_uuid = validated_data.get('entitlement_uuid', None)
+            if not entitlement_uuid:
+                raise ValidationError('Entitlement uuid is required for Entitlement Fulfillment.')
+
+        fulfillment_completed_update_ct_line_item_signal.send_robust(
+            sender=self.__class__,
+            **validated_data
+        )
+
+        return Response(
+            {'message': 'Order Fulfillment Response event processed successfully.'},
+            status=status.HTTP_200_OK
+        )
