b55c431e9b70483c4983fd3a685238753ccb9908
Nawfal Ahmed <nawfal.ahmed@arbisoft.com>
2025-05-02 19:08:32 +0500

    feat: use single api

--- commerce_coordinator/apps/iap/api/v1/urls.py
+++ commerce_coordinator/apps/iap/api/v1/urls.py
@@ -5,10 +5,9 @@ Defines routes for creating orders and other IAP-related endpoints.
 """
 
 from django.urls import path
-from commerce_coordinator.apps.iap.api.v1.views import CreateOrderView, PrepareCartView
+from commerce_coordinator.apps.iap.api.v1.views import CreateOrderView
 
 app_name = 'v1'
 urlpatterns = [
-    path('prepare-cart/', PrepareCartView.as_view(), name='prepare_cart'),
     path('create-order/', CreateOrderView.as_view(), name='create_order'),
 ]
