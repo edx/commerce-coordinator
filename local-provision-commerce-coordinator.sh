b98f2c8b066e87d1ee2e457a95f5becd68a5ee77
Muhammad Noyan  Aziz <noyan.aziz@A006-01474.local>
2025-06-05 10:20:33 +0500

    chore: update local-provision-commerce-coordinator.sh

--- local-provision-commerce-coordinator.sh
+++ local-provision-commerce-coordinator.sh
@@ -1,16 +1,52 @@
+#!/usr/bin/env bash
+
 name="commerce_coordinator"
 port="8140"
 
-ida_name="commerce-coordinator"
+RED="\x1B[31m"
+GREEN="\x1B[32m"
+NC="\x1B[0m"
+
+bash ./find-start-lms.sh
+
+# Install requirements
+# Can be skipped right now because we're using the --build flag on docker-compose. This will need to be changed once we move to devstack.
+
+# Wait for MySQL
+echo "Waiting for MySQL"
+until docker exec -i edx.devstack.mysql80 mysql -u root -se "SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'root')" &> /dev/null
+do
+  printf "."
+  sleep 1
+done
+sleep 5
+echo -e ""
+
+# Create the database
+docker exec -i edx.devstack.mysql80 mysql -u root -se "CREATE DATABASE commerce_coordinator;"
+
+# Run migrations
+echo -e "${GREEN}Running migrations for ${name}...${NC}"
+make migrate
 
 # Create superuser
 echo -e "${GREEN}Creating super-user for ${name}...${NC}"
-bash -c "echo 'from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(\"edx\", \"edx@example.com\", \"edx\") if not User.objects.filter(username=\"edx\").exists() else None' | python manage.py shell"
+python ./manage.py shell << EOIS
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+User.objects.create_superuser("edx", "edx@example.com", "edx") if not User.objects.filter(username="edx").exists() else None;
+EOIS
 
-# Provision IDA User in LMS
+lms_exec() {
+    docker exec -t edx.devstack.lms  bash -c "source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker ${*}"
+}
+
+
+# Provision IDA User in LMS (https://2u-internal.atlassian.net/wiki/spaces/SRE/pages/19267432/Setup+OAuth+Client+and+JWT+for+Internal+Services+Django+Oauth+Toolkit+version)
 echo -e "${GREEN}Provisioning ${name}_worker in LMS...${NC}"
-docker exec -t edx.devstack.lms  bash -c "source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker manage_user ${name}_worker ${name}_worker@example.com --staff --superuser"
+lms_exec "manage_user ${name}_worker ${name}_worker@example.com --staff --superuser"
+lms_exec "create_dot_application --grant-type authorization-code --skip-authorization --redirect-uris 'http://localhost:${port}/complete/edx-oauth2/' --client-id '${name}-sso-key' --client-secret '${name}-sso-secret' --scopes 'user_id' ${name}-sso ${name}_worker"
+lms_exec "create_dot_application --grant-type client-credentials --client-id '${name}-backend-service-key' --client-secret '${name}-backend-service-secret' ${name}-backend-service ${name}_worker"
 
-# Create the DOT applications - one for single sign-on and one for backend service IDA-to-IDA authentication.
-docker exec -t edx.devstack.lms  bash -c "source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker create_dot_application --grant-type authorization-code --skip-authorization --redirect-uris 'http://localhost:${port}/complete/edx-oauth2/' --client-id '${ida_name}-sso-key' --client-secret '${ida_name}-sso-secret' --scopes 'user_id' ${ida_name}-sso ${name}_worker"
-docker exec -t edx.devstack.lms bash -c "source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=devstack_docker create_dot_application --grant-type client-credentials --client-id '${ida_name}-backend-service-key' --client-secret '${ida_name}-backend-service-secret' ${ida_name}-backend-service ${name}_worker"
+echo -e "${GREEN}Processing static files...${NC}"
+python ./manage.py collectstatic
